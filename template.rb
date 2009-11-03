907f40e485e811810404e116f4f9d30e9079502e
Matt Mitchell <goodieboy@gmail.com>
2009-11-02 20:18:43 -0500

    changed comment

--- template.rb
+++ template.rb
@@ -18,7 +18,7 @@ tag = nil
 # git_export expects a full git repo url, and an optional branch name.
 # It will clone the repo, checkout the remote branch and then remote the .git file.
 #
-# Example: git_export 'git://github.com/projectblacklight/blacklight.git', 'release-2.4'
+# Example: git_export 'git://github.com/projectblacklight/blacklight.git', :tag=>'v2.4.0'
 #
 def git_export repo, new_dir_name=nil, opts={}
   if File.exists? 'vendor/plugins/blacklight' and (new_dir_name == 'vendor/plugins/blacklight')
