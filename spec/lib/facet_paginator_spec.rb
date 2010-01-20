8e6685e7d1352d1e4d66cc0d2235e38a7a386794
Jonathan Rochkind <rochkind@xs001.mse.jhu.edu>
2010-01-20 17:29:19 -0500

    rspect for Facet::Paginator object. Start with tests for has_next? and has_previous? behaving properly; has_next was previously buggy

---
+++ spec/lib/facet_paginator_spec.rb
@@ -0,0 +1,33 @@
+# Spec tests for Paginator class found in lib/blacklight/solr/facet_paginator.rb
+
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe 'Blacklight::Solr::Facets::Paginator' do
+  before(:all) do
+    require 'yaml'
+    @seven_facet_values = YAML::load("--- \n- !ruby/object:RSolr::Ext::Response::Facets::FacetItem \n  hits: 792\n  value: Book\n- !ruby/object:RSolr::Ext::Response::Facets::FacetItem \n  hits: 65\n  value: Musical Score\n- !ruby/object:RSolr::Ext::Response::Facets::FacetItem \n  hits: 58\n  value: Serial\n- !ruby/object:RSolr::Ext::Response::Facets::FacetItem \n  hits: 48\n  value: Musical Recording\n- !ruby/object:RSolr::Ext::Response::Facets::FacetItem \n  hits: 37\n  value: Microform\n- !ruby/object:RSolr::Ext::Response::Facets::FacetItem \n  hits: 27\n  value: Thesis\n- !ruby/object:RSolr::Ext::Response::Facets::FacetItem \n  hits: 0\n  value: \n")
+    @six_facet_values = @seven_facet_values.slice(1,6)
+    @limit = 6
+  end
+
+  it 'should have next when there are limit+1 results' do
+    paginator = Blacklight::Solr::Facets::Paginator.new(@seven_facet_values, 0, @limit)
+    
+    paginator.should be_has_next
+  end
+  it 'should not have next when there are fewer results' do
+    paginator = Blacklight::Solr::Facets::Paginator.new(@six_facet_values, 0, @limit)
+
+    paginator.should_not be_has_next
+  end
+  it 'should have previous when offset is greater than 0' do
+    paginator = Blacklight::Solr::Facets::Paginator.new(@seven_facet_values, 10, @limit)
+
+    paginator.should be_has_previous
+  end
+  it 'should not have previous when offset is 0' do
+    paginator = Blacklight::Solr::Facets::Paginator.new(@seven_facet_values, 0, @limit)
+
+    paginator.should_not be_has_previous
+  end
+end
