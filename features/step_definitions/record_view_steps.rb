fa5a0c834e8e393de227427fd1d00852434dd5ff
Jessie Keck <jkeck@SUL-AC-TP-jk-2.local>
2010-01-22 15:02:59 -0800

    Adding in Librarian View. The link is to the right of the SMS This link in the show_tools partial The link will pop up a lightbox that has the new marc_view partial rendered inside of it A new feature for the record view (CatalogController#show) has been added. The new feature checks the elements of the librarian view as well as checks some other display fields of the record.

---
+++ features/step_definitions/record_view_steps.rb
@@ -0,0 +1,7 @@
+Then /^I (should|should not) see an? "([^\"]*)" element containing "([^\"]*)"$/ do |bool,elem,content|
+  if bool == "should"
+    response.should have_selector("#{elem}",:content => content)
+  else
+    response.should_not have_selector("#{elem}",:content => content)
+  end
+end
