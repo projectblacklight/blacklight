2d8d20c81acd54fcb067dc9dff7e527aeb49559b
Matt Mitchell <goodieboy@gmail.com>
2010-02-12 10:45:24 -0500

    removed branch variable -- not used

--- template.rb
+++ template.rb
@@ -4,7 +4,6 @@ puts "\n* Blacklight Rails Template \n\n"
 # We do this because the repo name is "blacklight-plugin", which we don't want as the directory name.
 bl_dirname = 'blacklight'
 
-branch = nil
 tag = nil
 
 # install the blacklight plugin - remove this when the github move is complete!
