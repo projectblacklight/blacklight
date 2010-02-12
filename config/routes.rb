b9a98f956466559ad142610bbd2985d32695cbb9
Matt Mitchell <goodieboy@gmail.com>
2010-02-12 14:30:08 -0500

    added logic to prevent loading routes if running inside of application

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   
-  # to build Blacklight routes, pass the map object to the build method:
-  Blacklight::Routes.build map
+  # to build Blacklight routes, pass the map object to the build method.
+  # only build the routes if the plugin is running as the app.
+  Blacklight::Routes.build(map) if Rails.root.to_s == Blacklight.root.to_s
   
 end
