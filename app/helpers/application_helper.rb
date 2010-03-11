cf1ac83940dfc2785d12fe2eb1585adb52d50f82
Jonathan Rochkind <rochkind@xs001.mse.jhu.edu>
2010-03-11 10:26:21 -0500

    Move individual facet rendering code into helper methods render_facet_value and render_selected_facet_value.
    This DRYs up code between the sidebar _facets partial and the catalog/facet expanded facet list.
    It also provides useful hooks for over-riding facet item display in a local app or subsidiary plugin,
    which is useful to have.
    
    Even though these are helper methods, since they generate URLs known issues with rspec make
    it challenging to test them with rspec. We'll have to relie on cucumber.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -211,6 +211,25 @@ module ApplicationHelper
   #
   # facet param helpers ->
   #
+
+  # Standard display of a facet value in a list. Used in both _facets sidebar
+  # partial and catalog/facet expanded list. Will output facet value name as
+  # a link to add that to your restrictions, with count in parens. 
+  # first arg item is a facet value item from rsolr-ext.
+  # options consist of:
+  # :suppress_link => true # do not make it a link, used for an already selected value for instance
+  def render_facet_value(facet_solr_field, item, options ={})    
+    link_to_unless(options[:suppress_link], item.value, add_facet_params_and_redirect(facet_solr_field, item.value)) + " (" + format_num(item.hits) + ")" 
+  end
+
+  # Standard display of a SELECTED facet value, no link, special span
+  # with class, and 'remove' button.
+  def render_selected_facet_value(facet_solr_field, item)
+    '<span class="selected">' +
+    render_facet_value(facet_solr_field, item, :suppress_link => true) +
+    '</span>' +
+    ' [' + link_to("remove", remove_facet_params(facet_solr_field, item.value, params), :class=>"remove") + ']'
+  end
   
   # adds the value and/or field to params[:f]
   # Does NOT remove request keys and otherwise ensure that the hash
@@ -254,6 +273,7 @@ module ApplicationHelper
   # removes the field if there are no more values in params[:f][field]
   # removes additional params (page, id, etc..)
   def remove_facet_params(field, value, source_params=params)
+    debugger
     p = source_params.dup.symbolize_keys!
     # need to dup the facet values too,
     # if the values aren't dup'd, then the values
