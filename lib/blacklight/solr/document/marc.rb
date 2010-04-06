229a5c0685cf613f5ebc6b5c587bcbc8cade2ca1
Jonathan Rochkind <rochkind@xs001.mse.jhu.edu>
2010-04-05 21:15:17 -0400

    properly fetch marc from specified Solr field

--- lib/blacklight/solr/document/marc.rb
+++ lib/blacklight/solr/document/marc.rb
@@ -40,10 +40,10 @@ module Blacklight::Solr::Document::Marc
   def load_marc
       case _marc_format_type.to_s
         when 'marcxml'
-          records = MARC::XMLReader.new(StringIO.new(@marc_data)).to_a
+          records = MARC::XMLReader.new(StringIO.new( fetch(_marc_source_field) )).to_a
           return records[0]
         when 'marc21'
-          return MARC::Record.new_from_marc(@marc_data)          
+          return MARC::Record.new_from_marc( fetch(_marc_source_field) )          
         else
           raise UnsupportedMarcFormatType.new("Only marcxml and marc21 are supported.")
       end      
