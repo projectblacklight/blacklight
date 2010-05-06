712de88437ec0f17f8d906cee732d299745428df
Jonathan Rochkind <rochkind@xs001.mse.jhu.edu>
2010-05-06 15:12:53 -0400

    fix spec to look for new versions of jquery/jquery-ui

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -12,9 +12,11 @@ describe ApplicationController do
         controller.send(:default_html_head)
       end
       it "should setup js and css defaults" do
-        controller.javascript_includes.should include(["jquery-1.3.1.min.js", "jquery-ui-1.7.2.custom.min.js", "blacklight", "application", "accordion", "lightbox", {:plugin=>:blacklight}])
+        controller.javascript_includes.should find do |item|
+          item == ["jquery-1.4.2.min.js", "jquery-ui-1.7.2.custom.min.js", "blacklight", "application", "accordion", "lightbox", {:plugin=>:blacklight}]
+        end
 
-        controller.stylesheet_links.should include(["yui", "jquery/ui-lightness/jquery-ui-1.7.2.custom.css", "application", {:media=>"all", :plugin=>:blacklight}])
+        controller.stylesheet_links.should include(["yui", "jquery/ui-lightness/jquery-ui-1.8.1.custom.css", "application", {:media=>"all", :plugin=>:blacklight}])
       end
     end        
   end
