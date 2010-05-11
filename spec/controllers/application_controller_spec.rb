7a469f583634ee44f712d1c90352fb8f51c18e9b
Jessie Keck <jessie.keck@gmail.com>
2010-05-11 11:18:29 -0700

    Removing old lightbox JS

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -12,7 +12,7 @@ describe ApplicationController do
         controller.send(:default_html_head)
       end
       it "should setup js and css defaults" do
-        controller.javascript_includes.should include(["jquery-1.4.2.min.js", "jquery-ui-1.8.1.custom.min.js", "blacklight", "application", "accordion", "lightbox", {:plugin=>:blacklight}])#find do |item|
+        controller.javascript_includes.should include(["jquery-1.4.2.min.js", "jquery-ui-1.8.1.custom.min.js", "blacklight", "application", "accordion", {:plugin=>:blacklight}])#find do |item|
         #  item == ["jquery-1.4.2.min.js", "jquery-ui-1.7.2.custom.min.js", "blacklight", "application", "accordion", "lightbox", {:plugin=>:blacklight}]
         #end
 
