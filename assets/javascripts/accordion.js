e0d40d5e8fdffd8421fbd2ee9a9497b49d723215
Jonathan Rochkind <jonathan@dnil.net>
2010-07-15 13:48:08 -0400

    change accordion.js to toggle a div immediately following an h3, not just a ul. In case a plugin displays a limit using a div instead of a ul

--- assets/javascripts/accordion.js
+++ assets/javascripts/accordion.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
- $('#facets ul').each(function(){
-   var ul = $(this);
-   // find all ul's that don't have any span descendants with a class of "selected"
-   if($('span.selected', ul).length == 0){
+    $('#facets ul, #facets h3 + form').each(function(){
+   var f_content = $(this);
+   // find all f_content's that don't have any span descendants with a class of "selected"
+   if($('span.selected', f_content).length == 0){
         // hide it
-        ul.hide();
+        f_content.hide();
         // attach the toggle behavior to the h3 tag
-        $('h3', ul.parent()).click(function(){
-           // toggle the next ul sibling
-           $(this).next('ul').slideToggle();
+        $('h3', f_content.parent()).click(function(){
+           // toggle the content
+           $(f_content).slideToggle();
        });
    }
 });
