06434804b2ddcb109badba89146506a9d20e24e8
Chris Beer <chris@authoritativeopinion.com>
2010-11-01 14:51:53 -0400

    add css classes for 'twiddle' states

--- assets/javascripts/accordion.js
+++ assets/javascripts/accordion.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('#facets h3').next("ul, div").each(function(){
    var f_content = $(this);
+   $(f_content).prev('h3').addClass('twiddle');
    // find all f_content's that don't have any span descendants with a class of "selected"
    if($('span.selected', f_content).length == 0){
         // hide it
@@ -8,6 +9,7 @@ $(document).ready(function() {
         // attach the toggle behavior to the h3 tag
         $('h3', f_content.parent()).click(function(){
            // toggle the content
+	   $(this).toggleClass('twiddle-open');
            $(f_content).slideToggle();
        });
    }
