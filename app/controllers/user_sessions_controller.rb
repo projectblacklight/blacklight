08d5e77a0e0208b60c3dff8b3f962b789f406b45
Chris Beer <chris_beer@wgbh.org>
2010-11-29 16:36:11 -0500

    consolidate referer logic into a private controller and write some quick controller docs

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,48 +1,46 @@
 class UserSessionsController < ApplicationController
-#   before_filter :require_no_user, :only => [:new, :create]
-#   before_filter :require_user, :only => :destroy
-  
-#   def create
-#     if user = User.authenticate(params[:email], params[:password])
-#       self.current_user = user
-#       user.update_attribute(:last_login, Time.now)
-#       flash[:notice] = "Welcome #{user.login}!"
-#       redirect_to root_path
-#     else
-#       flash.now[:error] =  "Couldn't locate a user with those credentials"
-#       render :action => :new
-#     end
-#   end
-#   
-#   def destroy
-#     session[:user_id] = nil
-#     flash[:notice] = "You have successfully logged out."
-#     redirect_to root_path
-#   end
-  
+
+  ##
+  # The login form.
+  #
   def new
     @user_session = UserSession.new
+    @referer = referer_url
   end
 
+  ##
+  # The login action. If the login is successful, the user is re-directed to 
+  # the referring page, which should persisted as an HTTP parameter.
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Welcome #{@user_session.login}!"
-      redirect_to redirect_path
+      redirect_to referer_url
     else
       flash.now[:error] =  "Couldn't locate a user with those credentials"
       render :action => :new
     end
   end
 
+
+  ##
+  # The logout action. The user is redirected to the previous page,
+  # given as an HTTP parameter /or/ from the referer header.
   def destroy
-    current_user_session.destroy
+    current_user_session.destroy rescue nil
     flash[:notice] = "You have successfully logged out."
-    redirect_to redirect_path
+    redirect_to referer_url
   end
 
   private 
-  def redirect_path
+  ##
+  # Returns a url (or the root path, by default) to the page from which a user
+  # began the login/logout process. It examines the referer (sic) HTTP param,
+  # as well as the request's referer to determine the correct page. 
+  #
+  # `redirect_path` performs some basic checks to ensure the URL is internal
+  #  and will not cause redirect loops.
+  def referer_url
     referer = params[:referer] if params[:referer]
     referer ||= request.referer if request.referer
     referer &&= referer.sub(Regexp.new("^http://#{request.env["HTTP_HOST"]}"), '') if request.env["HTTP_HOST"]
--- app/helpers/user_sessions_helper.rb
+++ app/helpers/user_sessions_helper.rb
@@ -1,7 +1,2 @@
 module UserSessionsHelper
-  def referer_url
-    url = params[:referer]
-    url ||= request.referer
-    return url
-  end
 end
