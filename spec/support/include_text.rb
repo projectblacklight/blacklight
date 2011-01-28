1263c2691afe69b03a479ce36379d5947b13bdb6
Dan Funk <dhf8r@virginia.edu>
2011-01-28 13:36:11 -0500

    These are support files for rspec that helped resolve several problems / adds backwards compatibility for the rspec tests.  http://www.arctickiwi.com/blog/upgrading-to-rspec-2-with-ruby-on-rails-3

---
+++ spec/support/include_text.rb
@@ -0,0 +1,20 @@
+# Added from http://www.arctickiwi.com/blog/upgrading-to-rspec-2-with-ruby-on-rails-3
+module RSpec::Rails
+  module Matchers
+    RSpec::Matchers.define :include_text do |text|
+      match do |response_or_text|
+        @content = response_or_text.respond_to?(:body) ? response_or_text.body : response_or_text
+        @content.include?(text)
+      end
+
+      failure_message_for_should do |text|
+        "expected '#{@content}' to contain '#{text}'"
+      end
+
+      failure_message_for_should_not do |text|
+        "expected #{@content} to not contain '#{text}'"
+      end
+    end
+  end
+end
+
