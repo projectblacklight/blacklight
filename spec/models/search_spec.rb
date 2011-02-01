adc5686c17485f5ef4b35c79f370153a2c09c675
Jessie Keck <jessie.keck@gmail.com>
2011-01-31 19:58:48 -0800

    Fixing the date format the delete_old_searches spec was using.

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -41,10 +41,10 @@ describe Search do
     it "should destroy searches with no user_id that are older than X days" do
       Search.destroy_all
       days_old = 7
-      unsaved_search_today = Search.create(:user_id => nil, :created_at => Time.now)
-      unsaved_search_past = Search.create(:user_id => nil, :created_at => Time.now - (days_old + 1).days)
-      saved_search_today = Search.create(:user_id => 1, :created_at => Time.now)
-      saved_search_past = Search.create(:user_id => 1, :created_at => (Time.now - (days_old + 1).days))
+      unsaved_search_today = Search.create(:user_id => nil, :created_at => Date.today)
+      unsaved_search_past = Search.create(:user_id => nil, :created_at => Date.today - (days_old + 1).days)
+      saved_search_today = Search.create(:user_id => 1, :created_at => Date.today)
+      saved_search_past = Search.create(:user_id => 1, :created_at => (Date.today - (days_old + 1).days))
       lambda do
         Search.delete_old_searches(days_old)
       end.should change(Search, :count).by(-1)
