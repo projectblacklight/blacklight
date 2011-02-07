8d1de8eb3ff79064f489a22fe42a1c1b356b9b88
Dan Funk <dhf8r@virginia.edu>
2011-02-07 16:26:11 -0500

    changes to correct for the auto-html escaping that now happens in rails3 - The method symbolize_keys! is gone in rails 3.0.3 (it was still there and undepricated in rails 3.0.0 - but when I found the source, it was just returning a pointer to itself then anyway, so I just removed calls to this method).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -261,7 +261,7 @@ module ApplicationHelper
 
   def render_field_value value=nil
     value = [value] unless value.is_a? Array
-    return value.map { |v| html_escape v }.join field_value_separator 
+    return value.map { |v| v.html_safe }.join field_value_separator 
   end  
 
   def field_value_separator
@@ -288,7 +288,7 @@ module ApplicationHelper
   
   # Search History and Saved Searches display
   def link_to_previous_search(params)
-    link_to(render_search_to_s(params), catalog_index_path(params))
+    link_to(raw(render_search_to_s(params)), catalog_index_path(params)).html_safe
   end
   
   
@@ -364,11 +364,11 @@ module ApplicationHelper
   # removes the field if there are no more values in params[:f][field]
   # removes additional params (page, id, etc..)
   def remove_facet_params(field, value, source_params=params)
-    p = source_params.dup.symbolize_keys!
+    p = source_params.dup
     # need to dup the facet values too,
     # if the values aren't dup'd, then the values
     # from the session will get remove in the show view...
-    p[:f] = p[:f].dup.symbolize_keys!
+    p[:f] = p[:f].dup
     p.delete :page
     p.delete :id
     p.delete :counter
@@ -417,7 +417,7 @@ module ApplicationHelper
   # so we only need the +counter+ param here. We also need to know if we are viewing to document as part of search results.
   def link_to_document(doc, opts={:label=>Blacklight.config[:index][:show_link].to_sym, :counter => nil, :results_view => true})
     label = render_document_index_label doc, opts
-    link_to_with_data(label, catalog_path(doc[:id]), {:method => :put, :class => label.parameterize, :data => opts})
+    link_to_with_data(label, catalog_path(doc[:id]), {:method => :put, :class => label.parameterize, :data => opts}).html_safe
   end
 
   # link_back_to_catalog(:label=>'Back to Search')
