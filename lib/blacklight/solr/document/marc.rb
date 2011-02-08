9dd7f5a94d8055846e1e31df430f5cc9fc888338
Dan Funk <dhf8r@virginia.edu>
2011-02-08 11:47:02 -0500

    Moved the runtime error definition inside the module - rather than leaving it global. Made the error message slightly more verbose - which helped me as I was deubugging something.

--- lib/blacklight/solr/document/marc.rb
+++ lib/blacklight/solr/document/marc.rb
@@ -1,5 +1,3 @@
-class UnsupportedMarcFormatType < RuntimeError; end
-
 
 # This is a document extension meant to be mixed into a
 # Blacklight::Solr::Document class, such as SolrDocument. It provides support
@@ -18,9 +16,11 @@ class UnsupportedMarcFormatType < RuntimeError; end
 # SolrDocument.extension_paramters[:marc_source_field] = "name_of_solr_stored_field"
 # SolrDocument.extension_parameters[:marc_format_type] = :marc21 # or :marcxml
 module Blacklight::Solr::Document::Marc
-  
-  include Blacklight::Solr::Document::MarcExport # All our export_as stuff based on to_marc. 
 
+  include Blacklight::Solr::Document::MarcExport # All our export_as stuff based on to_marc. 
+  
+  class UnsupportedMarcFormatType < RuntimeError; end
+    
   def self.extended(document)
     # Register our exportable formats, we inherit these from MarcExport    
     Blacklight::Solr::Document::MarcExport.register_export_formats( document )
@@ -53,18 +53,18 @@ module Blacklight::Solr::Document::Marc
   end
 
   def load_marc
-      case _marc_format_type.to_s
-        when 'marcxml'
-          records = MARC::XMLReader.new(StringIO.new( fetch(_marc_source_field) )).to_a
-          return records[0]
-        when 'marc21'
-          return MARC::Record.new_from_marc( fetch(_marc_source_field) )          
-        else
-          raise UnsupportedMarcFormatType.new("Only marcxml and marc21 are supported.")
-      end      
-   end
-
-
+    case _marc_format_type.to_s
+    when 'marcxml'
+      records = MARC::XMLReader.new(StringIO.new( fetch(_marc_source_field) )).to_a
+      return records[0]
+    when 'marc21'
+      return MARC::Record.new_from_marc( fetch(_marc_source_field) )          
+    else
+      raise UnsupportedMarcFormatType.new("Only marcxml and marc21 are supported, this documents format is #{_marc_format_type}")
+    end      
+  end
+  
+  
   
   def _marc_helper
     @_marc_helper ||= (
