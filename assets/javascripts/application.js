fe3f84c007918da2a7bbe22d42f46b00cf5422bb
Jonathan Rochkind <jonathan@dnil.net>
2011-03-01 16:50:47 -0500

    Move per-page/sort behavior to progressive enhanced JS
    Remove in-line onchange=, instead provide JS behavior progressively enhanced in application.js.  JS behavior is to hide the submit button, and make the select menu auto-submit the form. Sponsored by the committee to make all blacklight JS progressively enhanced JS.

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -43,6 +43,8 @@ Blacklight = {};
 $(document).ready(function() {
   Blacklight.do_zebra_stripe();  
   
+  Blacklight.do_select_submit();
+  
   Blacklight.do_more_facets_behavior();
   
   Blacklight.do_lightbox_dialog();
@@ -74,6 +76,20 @@ $(document).ready(function() {
       });
     };
     Blacklight.do_more_facets_behavior.selector = "a.more_facets_link";
+    
+    // Used for sort-by and per-page controls, hide the submit button
+    // and make the select auto-submit
+    Blacklight.do_select_submit = function() {
+      $(Blacklight.do_select_submit.selector).each(function() {
+          var select = $(this);
+          select.closest("form").find("input[type=submit]").hide();
+          select.bind("change", function() {
+              this.form.submit();
+          });
+      });
+    };
+    Blacklight.do_select_submit.selector = "form.sort select, form.per_page select";
+    
     
     Blacklight.do_lightbox_dialog = function() {    
       $("a.lightboxLink").ajaxyDialog({
