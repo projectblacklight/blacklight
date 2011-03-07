fa2d9086590214389e05af6f1dc36b8254856e06
Chris Beer <chris@cbeer.info>
2011-03-07 17:45:46 -0500

    html_safe for #render_constraints

--- app/helpers/render_constraints_helper.rb
+++ app/helpers/render_constraints_helper.rb
@@ -11,7 +11,7 @@ module RenderConstraintsHelper
   # Render actual constraints, not including header or footer
   # info. 
   def render_constraints(localized_params = params)
-    render_constraints_query(localized_params) + render_constraints_filters(localized_params)
+    (render_constraints_query(localized_params) + render_constraints_filters(localized_params)).html_safe
   end
   
   def render_constraints_query(localized_params = params)
