6c6a8b6cf944e4fac78c6ad56feacc41136abbf0
Jessie Keck <jessie.keck@gmail.com>
2011-04-06 10:25:55 -0700

    No longer using export_as framework and now just simply using the doc ext.

--- spec/lib/blacklight_email_spec.rb
+++ spec/lib/blacklight_email_spec.rb
@@ -6,18 +6,18 @@ describe "Blacklight::Solr::Document::Email" do
   end
   it "should only return values that are available in the field semantics" do
     doc = SolrDocument.new({:id=>"1234", :title_display=>"My Title"})
-    email_body = doc.export_as_email_text
+    email_body = doc.to_email_text
     email_body.should match(/Title: My Title/)
     email_body.should_not match(/Author/)
   end
   it "should handle multi-values fields correctly" do
     doc = SolrDocument.new({:id=>"1234", :title_display=>["My Title", "My Alt. Title"]})
-    email_body = doc.export_as_email_text
+    email_body = doc.to_email_text
     email_body.should match(/Title: My Title My Alt. Title/)
   end
   it "should return nil if there are no valid field semantics to build the email body from" do
     doc = SolrDocument.new({:id=>"1234"})
-    doc.export_as_email_text.should be_nil
+    doc.to_email_text.should be_nil
   end
 end
 
