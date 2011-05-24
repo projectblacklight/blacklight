9c2934c40d2b69c0f0b70ce5f477750456520dd7
Chris Beer <chris_beer@wgbh.org>
2011-05-24 09:54:08 -0400

    move Blacklight route definitions into a separate module to make them more easily extended by plugins

--- lib/blacklight/routes.rb
+++ lib/blacklight/routes.rb
@@ -1,5 +1,6 @@
 module Blacklight
   class Routes
+
     def initialize(router, options)
       @router = router
       @options = options
@@ -25,63 +26,66 @@ module Blacklight
       [:bookmarks, :folders, :search_history, :saved_searches, :catalog, :feedback]
     end
 
-    def bookmarks
-      add_routes do |options|
-        match "bookmarks/clear", :to => "bookmarks#clear", :as => "clear_bookmarks"
-        resources :bookmarks
+    module RouteSets
+      def bookmarks
+        add_routes do |options|
+          match "bookmarks/clear", :to => "bookmarks#clear", :as => "clear_bookmarks"
+          resources :bookmarks
+        end
       end
-    end
-
-
-    def folders
-      add_routes do |options|
-         match "folder/clear", :to => "folder#clear", :as => "clear_folder"
-        match "folder/destroy", :to => "folder#destroy"
-        resources :folder, :only => [:index, :update, :destroy] 
-      end
-    end
   
-    def search_history
-      add_routes do |options|
-        match "search_history",             :to => "search_history#index",   :as => "search_history"
-        match "search_history/clear",       :to => "search_history#clear",   :as => "clear_search_history"
-        match "search_history/destroy/:id", :to => "search_history#destroy", :as => "delete_search"
-      end
-    end
-
-
-    def saved_searches
-      add_routes do |options|
-        match "saved_searches/clear",       :to => "saved_searches#clear",   :as => "clear_saved_searches"
-        match "saved_searches",       :to => "saved_searches#index",   :as => "saved_searches"
-        match "saved_searches/save/:id",    :to => "saved_searches#save",    :as => "save_search"
-        match "saved_searches/forget/:id",  :to => "saved_searches#forget",  :as => "forget_search"
-      end
-    end
   
-    def catalog
-      add_routes do |options|
-        # Catalog stuff.
-        match 'catalog/opensearch', :as => "opensearch_catalog"
-        match 'catalog/citation', :as => "citation_catalog"
-        match 'catalog/email', :as => "email_catalog"
-        match 'catalog/sms', :as => "sms_catalog"
-        match 'catalog/endnote', :as => "endnote_catalog"
-        match 'catalog/send_email_record', :as => "send_email_record_catalog"
-        match "catalog/facet/:id", :to => 'catalog#facet', :as => 'catalog_facet'
-        match 'catalog/unapi', :to => "catalog#unapi", :as => 'unapi'
-        resources :catalog, :only => [:index, :show, :update]
-        match 'catalog/:id/librarian_view', :to => "catalog#librarian_view", :as => "librarian_view_catalog"
+      def folders
+        add_routes do |options|
+           match "folder/clear", :to => "folder#clear", :as => "clear_folder"
+          match "folder/destroy", :to => "folder#destroy"
+          resources :folder, :only => [:index, :update, :destroy] 
+        end
       end
-    end
-
+    
+      def search_history
+        add_routes do |options|
+          match "search_history",             :to => "search_history#index",   :as => "search_history"
+          match "search_history/clear",       :to => "search_history#clear",   :as => "clear_search_history"
+          match "search_history/destroy/:id", :to => "search_history#destroy", :as => "delete_search"
+        end
+      end
   
-    # Feedback
-    def feedback
-      add_routes do |options|
-        match "feedback", :to => "feedback#show"    
-        match "feedback/complete", :to => "feedback#complete"
+  
+      def saved_searches
+        add_routes do |options|
+          match "saved_searches/clear",       :to => "saved_searches#clear",   :as => "clear_saved_searches"
+          match "saved_searches",       :to => "saved_searches#index",   :as => "saved_searches"
+          match "saved_searches/save/:id",    :to => "saved_searches#save",    :as => "save_search"
+          match "saved_searches/forget/:id",  :to => "saved_searches#forget",  :as => "forget_search"
+        end
+      end
+    
+      def catalog
+        add_routes do |options|
+          # Catalog stuff.
+          match 'catalog/opensearch', :as => "opensearch_catalog"
+          match 'catalog/citation', :as => "citation_catalog"
+          match 'catalog/email', :as => "email_catalog"
+          match 'catalog/sms', :as => "sms_catalog"
+          match 'catalog/endnote', :as => "endnote_catalog"
+          match 'catalog/send_email_record', :as => "send_email_record_catalog"
+          match "catalog/facet/:id", :to => 'catalog#facet', :as => 'catalog_facet'
+          match 'catalog/unapi', :to => "catalog#unapi", :as => 'unapi'
+          resources :catalog, :only => [:index, :show, :update]
+          match 'catalog/:id/librarian_view', :to => "catalog#librarian_view", :as => "librarian_view_catalog"
+        end
+      end
+  
+    
+      # Feedback
+      def feedback
+        add_routes do |options|
+          match "feedback", :to => "feedback#show"    
+          match "feedback/complete", :to => "feedback#complete"
+        end
       end
     end
+    include RouteSets
   end
 end
