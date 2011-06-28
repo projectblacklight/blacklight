1f34677316fe4c06c906831e333ea99c714164d3
Dan Funk <dhf8r@virginia.edu>
2011-06-28 17:14:15 -0400

    Auto populate the user_type field in bookmarks and searches so that it correctly references the given model name for the user.  This adresses codebase-316

--- lib/generators/blacklight/templates/migrations/add_user_types_to_bookmarks_searches.rb
+++ lib/generators/blacklight/templates/migrations/add_user_types_to_bookmarks_searches.rb
@@ -3,6 +3,12 @@ class AddUserTypesToBookmarksSearches < ActiveRecord::Migration
   def self.up
     add_column :searches, :user_type, :string
     add_column :bookmarks, :user_type, :string
+    execute <<-SQL
+       UPDATE searches set user_type="<%=model_name%>"
+    SQL
+    execute <<-SQL
+       UPDATE bookmarks set user_type="<%=model_name%>"
+    SQL
   end
 
   def self.down
