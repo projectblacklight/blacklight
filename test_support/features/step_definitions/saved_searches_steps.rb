eb41f34090fea76c62537740c5238e5be1eca196
Chris Beer <chris@cbeer.info>
2011-12-23 10:28:26 -0500

    remove authlogic-specific login attribute (which is not provided by devise, and breaks in Rails 3.2)

--- test_support/features/step_definitions/saved_searches_steps.rb
+++ test_support/features/step_definitions/saved_searches_steps.rb
@@ -1,14 +1,11 @@
 # -*- encoding : utf-8 -*-
 Given /^I am logged in as "([^\"]*)"$/ do |login|
   email = "#{login}@#{login}.com"
-  user = User.create(:login => login, :email => email, :password => "password", :password_confirmation => "password")
-#  visit user_sessions_path(:user_session => {:login => login, :password => "password"}), :post
-#  User.find(user.id).should_not be_nil
+  user = User.create(:email => email, :password => "password", :password_confirmation => "password")
   visit new_user_session_path
   fill_in("user_email", :with => email) 
   fill_in("user_password", :with => "password") 
   click_button("Sign in")
-#  response.body.should =~ /Logged/m  
   step 'I should see "Log Out"'
 end
 
