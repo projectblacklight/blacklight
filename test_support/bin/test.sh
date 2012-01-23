f72090be49fefa8f883fa69f8a3caf6f2ed50d6c
Chris Beer <chris_beer@wgbh.org>
2012-01-23 16:42:25 -0500

    cleanup test.sh script

--- test_support/bin/test.sh
+++ test_support/bin/test.sh
@@ -2,6 +2,10 @@
 # test.sh
 # Create a default rails appliaction, install blacklight, and run all the tests.
 
+#If null or empty, use default value
+RAILS_VERSION=${RAILS_VERSION:-3.2}
+JETTY_URL=${JETTY_URL:-"https://github.com/projectblacklight/blacklight-jetty/zipball/v3.5.0"}
+
 before="$(date +%s)"
 benchmark() 
 {
@@ -47,12 +51,11 @@ else
   exit 1
 fi
 
-
 rvm use "$@" --create
 check_errs $? "rvm failed.  please run 'rvm install $@', and then re-run these tests." 
 
-if ! gem query -n rails -v "~>3.1" --installed > /dev/null; then
-  gem install --no-rdoc --no-ri 'rails'
+if ! gem query -n rails -v $RAILS_VERSION --installed > /dev/null; then
+  gem install --no-rdoc --no-ri 'rails' -v $RAILS_VERSION
 fi
 
 if ! gem query -n bundler -v ">=1.0" --installed > /dev/null; then
@@ -61,77 +64,70 @@ fi
 
 rails new test_app
 cd test_app
+rm public/index.html
+
 echo "
-source 'http://rubygems.org'
-gem 'rack'
-gem 'rails', '~> 3.1'
 platforms :jruby do
   gem 'jruby-openssl'
   gem 'activerecord-jdbcsqlite3-adapter'
   gem 'jdbc-sqlite3'
   gem 'mediashelf-loggable', '>= 0.4.8'
+  gem 'therubyrhino'
 end
+
 platforms :ruby do
  gem 'sqlite3-ruby', :require => 'sqlite3'
+ gem 'execjs'
+ gem 'therubyracer'
 end
+
 gem 'blacklight', :path => '../../'
 gem 'jquery-rails'
 
 group :assets do
-  gem 'sass-rails', '~> 3.1'
-  gem 'coffee-rails', '~> 3.1'
-  gem 'uglifier'
   gem 'compass', '0.12.alpha.2'
-   
-  platforms :ruby do
-    gem 'execjs'
-    gem 'therubyracer'
-  end
-
-  platforms :jruby do
-    gem 'therubyrhino'
-  end
 end
 
 
 # For testing
 group :development, :test do 
-       gem 'rspec'
-       gem 'rspec-rails', '>=2.5.0'       
-       gem 'generator_spec'
-       gem 'cucumber-rails'
-       gem 'database_cleaner'  
-       gem 'capybara'
-       gem 'webrat'
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'generator_spec'
+  gem 'cucumber-rails'
+  gem 'database_cleaner'
+  gem 'capybara'
 end
 
 gem 'jettywrapper', '>= 1.2.0'
 gem \"devise\"
-" > Gemfile
+" >> Gemfile
 
-bundle install --local &> /dev/null 
-bundle update
-# If a local install fails, try a full install.
-if [ "$?" -ne "0" ]
-then
-  bundle install
-fi
+bundle install
 check_errs $? "Bundle install failed." 
+
 rails generate blacklight -d
 check_errs $?  "Blacklight generator failed" 
+
 bundle exec rake db:migrate
 check_errs $? "Rake Migration failed" 
+
 rails g cucumber:install &> /dev/null 
-jetty_zip="/tmp/bl_jetty_3_5_0.zip"
+
+
+jetty_zip=$( echo $JETTY_URL | awk '{split($0,a,"/"); print "/tmp/blacklight_jetty_"a[length(a)]}')
 if [ ! -f $jetty_zip ]
 then
-  curl -L https://github.com/projectblacklight/blacklight-jetty/zipball/v3.5.0 -o $jetty_zip
+  curl -L $JETTY_URL -o $jetty_zip
   check_errs $? "Jetty file does not exist, and cannot be downloaded."
 fi
+
 rails g blacklight:jetty test_jetty -e test -d $jetty_zip
-  check_errs $? "Jetty setup failed."
-rm public/index.html
+check_errs $? "Jetty setup failed."
+
 bundle exec rake solr:marc:index_test_data RAILS_ENV=test
+
 bundle exec rake blacklight:hudson
 check_errs $? "Tests failed." 
+
 benchmark
