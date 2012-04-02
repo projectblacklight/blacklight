972668e3fff4bdbcc4956102d6496628ef92cb9e
Chris Beer <chris_beer@wgbh.org>
2012-04-02 10:04:02 -0400

    actually create a test document for the crazy-long-title tests

--- test_support/spec/helpers/blacklight_helper_spec.rb
+++ test_support/spec/helpers/blacklight_helper_spec.rb
@@ -268,10 +268,12 @@ describe BlacklightHelper do
      end
 
      it "passes on the title attribute to the link_to_with_data method" do
+       @document = SolrDocument.new('id'=>'123456')
        link_to_document(@document,:label=>"Some crazy long label...",:title=>"Some crazy longer label").should match(/title=\"Some crazy longer label\"/)
      end
 
      it "doesn't add an erroneous title attribute if one isn't provided" do
+       @document = SolrDocument.new('id'=>'123456')
        link_to_document(@document,:label=>"Some crazy long label...").should_not match(/title=/)
      end
    end
