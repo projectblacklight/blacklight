7649be52e990dd3aaac1d9659aee62282cf3bc90
Chris Beer <cabeer@stanford.edu>
2012-06-21 10:40:45 -0700

    s/blacklight.record_mailer.(email|sms)/blacklight.(email|sms).text

--- app/models/record_mailer.rb
+++ app/models/record_mailer.rb
@@ -4,7 +4,7 @@ class RecordMailer < ActionMailer::Base
   def email_record(documents, details, url_gen_params)
     #raise ArgumentError.new("RecordMailer#email_record only works with documents with a #to_marc") unless document.respond_to?(:to_marc)
         
-    subject = I18n.t('blacklight.record_mailer.email.subject', :count => documents.length, :title => (documents.first.to_semantic_values[:title] rescue 'N/A') )
+    subject = I18n.t('blacklight.email.text.subject', :count => documents.length, :title => (documents.first.to_semantic_values[:title] rescue 'N/A') )
 
     @documents      = documents
     @message        = details[:message]
