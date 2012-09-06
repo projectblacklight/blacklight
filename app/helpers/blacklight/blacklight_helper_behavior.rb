496e04e7dafb58f7d0f4028ec88a4bd0b46d7862
Ryan Ahearn <ryan@coshx.com>
2012-09-06 15:10:04 -0400

    Uses render_field_value to render the link_to_document label
    This allows highlighting of a field used for the link text when
    combined with the blacklight_highlight plugin

--- app/helpers/blacklight/blacklight_helper_behavior.rb
+++ app/helpers/blacklight/blacklight_helper_behavior.rb
@@ -260,10 +260,11 @@ module Blacklight::BlacklightHelperBehavior
   
   def render_document_index_label doc, opts
     label = nil
-    label ||= doc.get(opts[:label]) if opts[:label].instance_of? Symbol
+    label ||= doc.get(opts[:label], :sep => nil) if opts[:label].instance_of? Symbol
     label ||= opts[:label].call(doc, opts) if opts[:label].instance_of? Proc
     label ||= opts[:label] if opts[:label].is_a? String
     label ||= doc.id
+    render_field_value label
   end
 
   # link_to_document(doc, :label=>'VIEW', :counter => 3)
