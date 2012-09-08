8db1649bed0b36f458a571121b948f33a3506cb1
Chris Beer <cabeer@stanford.edu>
2012-09-08 15:38:54 -0400

    add devise test helpers to all controller tests

--- test_support/spec/controllers/application_controller_spec.rb
+++ test_support/spec/controllers/application_controller_spec.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe ApplicationController do
+  include Devise::TestHelpers
 
 # HEAD CONTENT
   describe "head content from variables" do
--- test_support/spec/controllers/search_history_controller_spec.rb
+++ test_support/spec/controllers/search_history_controller_spec.rb
@@ -2,6 +2,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe SearchHistoryController do
+  include Devise::TestHelpers
+
   describe "index" do
     before(:all) do
       @one = Search.create
