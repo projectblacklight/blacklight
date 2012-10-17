f2554c8d46c2df2c3097c915cae63af4abf7e6e9
Chris Beer <chris@cbeer.info>
2012-10-17 08:36:48 -0700

    split out #params_for_search, which sanitizes current request params for a refined search (e.g. removed pagination)

--- app/helpers/blacklight/blacklight_helper_behavior.rb
+++ app/helpers/blacklight/blacklight_helper_behavior.rb
@@ -284,16 +284,11 @@ module Blacklight::BlacklightHelperBehavior
     link_url = url_for(query_params)
     link_to opts[:label], link_url
   end
-  
-  # Create form input type=hidden fields representing the entire search context,
-  # for inclusion in a form meant to change some aspect of it, like
-  # re-sort or change records per page. Can pass in params hash
-  # as :params => hash, otherwise defaults to #params. Can pass
-  # in certain top-level params keys to _omit_, defaults to :page
-  def search_as_hidden_fields(options={})
-    
-    options = {:params => params, :omit_keys => [:page]}.merge(options)
-    my_params = options[:params].dup
+
+  def params_for_search(options={})
+    my_params = (options[:params] || params).dup
+    options[:omit_keys] ||= []
+
     options[:omit_keys].each do |omit_key|
       case omit_key
         when Hash
@@ -307,13 +302,26 @@ module Blacklight::BlacklightHelperBehavior
         else
           my_params.delete(omit_key)
       end
-    end
+    end    
+
     # removing action and controller from duplicate params so that we don't get hidden fields for them.
     my_params.delete(:action)
     my_params.delete(:controller)
     # commit is just an artifact of submit button, we don't need it, and
     # don't want it to pile up with another every time we press submit again!
     my_params.delete(:commit)
+
+    my_params
+  end
+  
+  # Create form input type=hidden fields representing the entire search context,
+  # for inclusion in a form meant to change some aspect of it, like
+  # re-sort or change records per page. Can pass in params hash
+  # as :params => hash, otherwise defaults to #params. Can pass
+  # in certain top-level params keys to _omit_, defaults to :page
+  def search_as_hidden_fields(options={})
+    my_params = params_for_search({:omit_keys => [:page]}.merge(options))
+
     # hash_as_hidden_fields in hash_as_hidden_fields.rb
     return hash_as_hidden_fields(my_params)
   end
@@ -341,7 +349,7 @@ module Blacklight::BlacklightHelperBehavior
     @template_format = old_format
     return result
   end
-  
+
   # puts together a collection of documents into one refworks export string
   def render_refworks_texts(documents)
     val = ''
