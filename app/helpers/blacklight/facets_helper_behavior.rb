543778bb78faa9d28fa4819cc1d8a2cb11c079ef
Chris Beer <chris@cbeer.info>
2012-10-22 15:21:56 -0700

    remove facet count .badge; instead float the count right.

--- app/helpers/blacklight/facets_helper_behavior.rb
+++ app/helpers/blacklight/facets_helper_behavior.rb
@@ -115,7 +115,7 @@ module Blacklight::FacetsHelperBehavior
   # Renders a count value for facet limits. Can be over-ridden locally
   # to change style. And can be called by plugins to get consistent display. 
   def render_facet_count(num)
-    content_tag("span", t('blacklight.search.facets.count', :number => num), :class => "badge") 
+    content_tag("span", t('blacklight.search.facets.count', :number => num), :class => "count") 
   end
   
   # adds the value and/or field to params[:f]
