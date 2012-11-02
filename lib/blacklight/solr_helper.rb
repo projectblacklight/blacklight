938ba9bf3c28b4c9f420dd21b29472c3207f20c3
Chris Beer <cabeer@stanford.edu>
2012-11-02 12:02:03 -0700

    fix #484, use top-level constant BLACKLIGHT_VERBOSE_LOGGING (uninitialized by default) to log the full solr response with each solr query

--- lib/blacklight/solr_helper.rb
+++ lib/blacklight/solr_helper.rb
@@ -360,7 +360,7 @@ module Blacklight::SolrHelper
 
     solr_response = find(blacklight_config.solr_request_handler, self.solr_search_params(user_params).merge(extra_controller_params))  
     document_list = solr_response.docs.collect {|doc| SolrDocument.new(doc, solr_response)}  
-    Rails.logger.debug("Solr response: #{solr_response.inspect}")
+    Rails.logger.debug("Solr response: #{solr_response.inspect}") if defined?(::BLACKLIGHT_VERBOSE_LOGGING) and ::BLACKLIGHT_VERBOSE_LOGGING
     Rails.logger.debug("Solr fetch: #{self.class}#get_search_results (#{'%.1f' % ((Time.now.to_f - bench_start.to_f)*1000)}ms)")
     
     return [solr_response, document_list]
