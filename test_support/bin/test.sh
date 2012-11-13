78d43d0bbab5596abcbbf93a37aeed21dcfdf7f9
Chris Beer <cabeer@stanford.edu>
2012-11-12 18:26:22 -0800

    make sure jetty is really stopped before running tests

--- test_support/bin/test.sh
+++ test_support/bin/test.sh
@@ -143,10 +143,10 @@ fi
 rails g blacklight:jetty test_jetty -e test -d $jetty_zip
 check_errs $? "Jetty setup failed."
 
-ln -s test_jetty jetty
 rake jetty:start RAILS_ENV=test
 bundle exec rake solr:marc:index_test_data RAILS_ENV=test
 rake jetty:stop
+sleep 2
 
 bundle exec rake blacklight:hudson RAILS_ENV=test
 check_errs $? "Tests failed." 
