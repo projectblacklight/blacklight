285019ba67ff589bca8603c789bd86b29329d611
Jessie Keck <jessie.keck@gmail.com>
2012-11-13 00:52:07 -0800

    Remove return false on li click event for css dropdown.

--- app/assets/javascripts/blacklight/css_dropdowns.js
+++ app/assets/javascripts/blacklight/css_dropdowns.js
@@ -2,7 +2,6 @@
 	Blacklight.add_css_dropdown_click_support = function() {
 		$(".css-dropdown li:not(.no-menu)").on('click',function(){
 			$(this).toggleClass("hovering");
-			return false;
 		});
 	};
 	$(document).ready(function() {
