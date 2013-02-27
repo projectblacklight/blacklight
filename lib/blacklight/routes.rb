192bf07a0f5ef84a1c5e1a6e453fabb7df7de34a
Chris Beer <cabeer@stanford.edu>
2013-02-26 19:01:07 -0800

    use specific request methods instead of 'match' (which is removed from Rails 4)

--- lib/blacklight/routes.rb
+++ lib/blacklight/routes.rb
@@ -30,43 +30,44 @@ module Blacklight
     module RouteSets
       def bookmarks
         add_routes do |options|
-          match "bookmarks/clear", :to => "bookmarks#clear", :as => "clear_bookmarks"
+          delete "bookmarks/clear", :to => "bookmarks#clear", :as => "clear_bookmarks"
           resources :bookmarks
         end
       end
   
       def search_history
         add_routes do |options|
-          match "search_history",             :to => "search_history#index",   :as => "search_history"
-          match "search_history/clear",       :to => "search_history#clear",   :as => "clear_search_history"          
+          get "search_history",             :to => "search_history#index",   :as => "search_history"
+          delete "search_history/clear",       :to => "search_history#clear",   :as => "clear_search_history"          
         end
       end
   
   
       def saved_searches
         add_routes do |options|
-          match "saved_searches/clear",       :to => "saved_searches#clear",   :as => "clear_saved_searches"
-          match "saved_searches",       :to => "saved_searches#index",   :as => "saved_searches"
-          match "saved_searches/save/:id",    :to => "saved_searches#save",    :as => "save_search"
-          match "saved_searches/forget/:id",  :to => "saved_searches#forget",  :as => "forget_search"
+          delete "saved_searches/clear",       :to => "saved_searches#clear",   :as => "clear_saved_searches"
+          get "saved_searches",       :to => "saved_searches#index",   :as => "saved_searches"
+          put "saved_searches/save/:id",    :to => "saved_searches#save",    :as => "save_search"
+          delete "saved_searches/forget/:id",  :to => "saved_searches#forget",  :as => "forget_search"
+          post "saved_searches/forget/:id",  :to => "saved_searches#forget",  :as => "forget_search"
         end
       end
     
       def catalog
         add_routes do |options|
           # Catalog stuff.
-          match 'catalog/opensearch', :as => "opensearch_catalog"
-          match 'catalog/citation', :as => "citation_catalog"
-          match 'catalog/email', :as => "email_catalog"
-          match 'catalog/sms', :as => "sms_catalog"
-          match 'catalog/endnote', :as => "endnote_catalog"
-          match 'catalog/send_email_record', :as => "send_email_record_catalog"
-          match "catalog/facet/:id", :to => 'catalog#facet', :as => 'catalog_facet'
+          get 'catalog/opensearch', :as => "opensearch_catalog"
+          get 'catalog/citation', :as => "citation_catalog"
+          get 'catalog/email', :as => "email_catalog"
+          get 'catalog/sms', :as => "sms_catalog"
+          get 'catalog/endnote', :as => "endnote_catalog"
+          get 'catalog/send_email_record', :as => "send_email_record_catalog"
+          get "catalog/facet/:id", :to => 'catalog#facet', :as => 'catalog_facet'
 
 
-          match "catalog", :to => 'catalog#index', :as => 'catalog_index'
+          get "catalog", :to => 'catalog#index', :as => 'catalog_index'
 
-          match 'catalog/:id/librarian_view', :to => "catalog#librarian_view", :as => "librarian_view_catalog"
+          get 'catalog/:id/librarian_view', :to => "catalog#librarian_view", :as => "librarian_view_catalog"
         end
       end
 
@@ -83,8 +84,8 @@ module Blacklight
       # Feedback
       def feedback
         add_routes do |options|
-          match "feedback", :to => "feedback#show"    
-          match "feedback/complete", :to => "feedback#complete"
+          get "feedback", :to => "feedback#show"    
+          get "feedback/complete", :to => "feedback#complete"
         end
       end
     end
