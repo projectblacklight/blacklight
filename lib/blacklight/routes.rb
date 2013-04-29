ca7f6e152f0776d0f124659646b8a456ac1ee58b
Chris Beer <chris@cbeer.info>
2013-04-29 09:36:23 -0700

    remove duplicate named routes (which pointed to the same path anyway..)

--- lib/blacklight/routes.rb
+++ lib/blacklight/routes.rb
@@ -49,7 +49,7 @@ module Blacklight
           get "saved_searches",       :to => "saved_searches#index",   :as => "saved_searches"
           put "saved_searches/save/:id",    :to => "saved_searches#save",    :as => "save_search"
           delete "saved_searches/forget/:id",  :to => "saved_searches#forget",  :as => "forget_search"
-          post "saved_searches/forget/:id",  :to => "saved_searches#forget",  :as => "forget_search"
+          post "saved_searches/forget/:id",  :to => "saved_searches#forget"
         end
       end
     
