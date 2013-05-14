547c19c2a6da091c229d58f12e69273d6b0194f5
alonyado <alonyado@gmail.com>
2013-05-14 19:59:38 +0300

    run query each time for solr + fix for bigger data
    increase acet.limit to 1000 in each request
    and requesting solr

--- app/assets/javascripts/solr-autocomplete/ajax-solr/widgets/AutocompleteWidget.js
+++ app/assets/javascripts/solr-autocomplete/ajax-solr/widgets/AutocompleteWidget.js
@@ -5,33 +5,58 @@ AjaxSolr.AutocompleteWidget = AjaxSolr.AbstractTextWidget.extend({
     $(this.target).find('input').unbind().removeData('events').val('');
 
     var self = this;
-    
+    self.cutList = 15
+    self.minLen = 0
+        
     var callback = function (response) {
-      var list = [];
-      for (var i = 0; i < self.fields.length; i++) {
-        var field = self.fields[i];
-        for (var facet in response.facet_counts.facet_fields[field]) {
-          list.push({
-            field: field,
-            value: facet,
-//          label: facet + ' (' + response.facet_counts.facet_fields[field][facet] + ') - ' + field
-            label: facet + '(' + response.facet_counts.facet_fields[field][facet] + ')'
-          });
-        }
-      }
-
+      
+       self.list = [];
+  	       for (var i = 0; i < self.fields.length; i++) {
+			var field = self.fields[i];
+			for (var facet in response.facet_counts.facet_fields[field]) {
+			  self.list.push({
+			    field: field,
+			    value: facet,
+			    label: facet + '(' + response.facet_counts.facet_fields[field][facet] + ')'
+			  });
+			}
+		      }
+         
+      
       self.requestSent = false;
       $(self.target).find('input').autocomplete('destroy').autocomplete({
         source: function(request, response) {
-        var results = $.ui.autocomplete.filter(list, request.term);
-        response(results.slice(0, 15));
+         var params = [ 'rows=0&facet=true&facet.limit=1000&facet.mincount=1&json.nl=map&facet.sort=count' ];
+    	 for (var i = 0; i < self.fields.length; i++) {
+	      params.push('facet.field=' + self.fields[i]);
+	 }
+         params.push('facet.prefix=' + request.term);
+         params.push('q=' + '');
+         $.getJSON(self.manager.solrUrl + 'select?' + params.join('&') + '&wt=json&json.wrf=?', request, 
+         function( data, status, xhr ) {
+		 self.list = [];
+		       for (var i = 0; i < self.fields.length; i++) {
+			var field = self.fields[i];
+			for (var facet in data.facet_counts.facet_fields[field]) {
+			  self.list.push({
+			    field: field,
+			    value: facet,
+			    label: facet + '(' + data.facet_counts.facet_fields[field][facet] + ')'
+			  });
+			}
+		      }	
+		 var results = self.list;
+		response(results.slice(0, self.cutList));
+		}
+         );
+        
 },
-        minLength:1,
+        minLength:self.minLen,
         select: function(event, ui) {
           if (ui.item) {
             self.requestSent = true;
             if (self.manager.store.addByValue('fq', ui.item.field + ':' + AjaxSolr.Parameter.escapeValue(ui.item.value))) {
-              self.doRequest();
+              //self.doRequest();
             }
           }
         }
@@ -40,7 +65,7 @@ AjaxSolr.AutocompleteWidget = AjaxSolr.AbstractTextWidget.extend({
       // This has lower priority so that requestSent is set.
       $(self.target).find('input').bind('keydown', function(e) {
         if (self.requestSent === false && e.which == 13) {
-          var value = $(this).val();
+          var value =wt=json& $(this).val();
           if (value && self.set(value)) {
             self.doRequest();
           }
@@ -48,7 +73,7 @@ AjaxSolr.AutocompleteWidget = AjaxSolr.AbstractTextWidget.extend({
       });
     } // end callback
 
-    var params = [ 'rows=0&facet=true&facet.limit=-1&facet.mincount=1&json.nl=map&facet.sort=count' ];
+    var params = [ 'rows=0&facet=true&facet.limit='  + self.cutList + '&facet.mincount=1&json.nl=map&facet.sort=count' ];
     for (var i = 0; i < this.fields.length; i++) {
       params.push('facet.field=' + this.fields[i]);
     }
