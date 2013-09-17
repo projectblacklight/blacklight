c3df9ee619aa08546190e889f53531dbcd4305f0
Chris Beer <cabeer@stanford.edu>
2013-09-17 12:14:11 -0400

    refactor Blacklight::User to actually use ActiveRecord methods

--- spec/lib/blacklight_user_spec.rb
+++ spec/lib/blacklight_user_spec.rb
@@ -2,76 +2,57 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "Blacklight::User" do
-  class MockUser
-    include Blacklight::User::InstanceMethods
-    attr_accessor :searches
-    attr_accessor :bookmarks
-  end
 
-  class MockBookmark
-    attr_accessor :document_id
+  subject { User.create! :email => 'xyz@example.com', :password => 'xyz12345' }
 
-    def initialize document_id
-      self.document_id = document_id
-    end
-  end
-
-  before(:each) do
-    @user = MockUser.new
-    @user.searches = []
-    @user.bookmarks = []
-  end
-
-  def mock_bookmark doc_id
-    MockBookmark.new doc_id
+  def mock_bookmark document_id
+    Bookmark.new :document_id => document_id
   end
 
   it "should know if it doesn't have bookmarks" do
-    @user.has_bookmarks?.should == false
+    expect(subject).to_not have_bookmarks
   end
 
   it "should know if it has bookmarkss" do
-    @user.bookmarks.push(mock_bookmark(1))
-    @user.bookmarks.push(mock_bookmark(2))
-    @user.has_bookmarks?.should == true
+    subject.bookmarks << mock_bookmark(1)
+    subject.bookmarks << mock_bookmark(2)
+
+    expect(subject).to have_bookmarks
   end
 
   it "should know if it has a bookmarked document" do
-    @user.bookmarks.push(mock_bookmark(1))
-    @user.should be_document_is_bookmarked(1)
-  end
-
-  it "should return a bookmark it a document is bookmarked" do
-    @user.bookmarks.push(mock_bookmark(1))
-    @user.existing_bookmark_for(1).should be_a_kind_of(MockBookmark)
+    subject.bookmarks << mock_bookmark(1)
+    expect(subject.document_is_bookmarked?(1)).to be_true
   end
 
   it "should be able to create bookmarks in batches" do
     @md1 = { :document_id => 1 }
     @md2 = { :document_id => 2 }
     @md3 = { :document_id => 3 }
-    @user.bookmarks.should_receive(:create).with(@md1)
-    @user.bookmarks.should_receive(:create).with(@md2)
-    @user.bookmarks.should_receive(:create).with(@md3)
 
-    @user.documents_to_bookmark= [@md1,@md2,@md3]
+    subject.documents_to_bookmark= [@md1,@md2,@md3]
+    expect(subject.bookmarks).to have(3).bookmarks
+    expect(subject.bookmarked_document_ids).to include("1","2","3")
+
   end
 
   it "should not recreate bookmarks for documents already bookmarked" do
-    @md1 = { :document_id => 1 }
-    @user.bookmarks.should_not_receive(:create).with(@md1)
+    subject.bookmarks << mock_bookmark(1)
 
-    @user.bookmarks.push(mock_bookmark(1))
-    @user.documents_to_bookmark=[@md1]
+    @md1 = { :document_id => 1 }
+    subject.bookmarks.should_not_receive(:create).with(@md1)
+
+    subject.bookmarks.push(mock_bookmark(1))
+    subject.documents_to_bookmark=[@md1]
   end
 
   it "should know if it doesn't have a search" do
-    @user.has_searches?.should == false
+    subject.has_searches?.should == false
   end
 
   it "should know if it has a search" do
-    @user.searches.push(1)
-    @user.has_searches?.should == true
+    subject.searches << Search.new
+    subject.has_searches?.should == true
   end
 
 end    
