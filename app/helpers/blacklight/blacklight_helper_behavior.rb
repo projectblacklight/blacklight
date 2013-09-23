57ccb7397fe71dbf538436ba8fc52d8d83dcaa2c
Chris Beer <cabeer@stanford.edu>
2013-09-23 13:28:21 -0700

    deprecate session[:search][:results_view], as any code consuming that in blacklight has been removed or disabled.

--- app/helpers/blacklight/blacklight_helper_behavior.rb
+++ app/helpers/blacklight/blacklight_helper_behavior.rb
@@ -477,10 +477,10 @@ module Blacklight::BlacklightHelperBehavior
   # Use the catalog_path RESTful route to create a link to the show page for a specific item.
   # catalog_path accepts a HashWithIndifferentAccess object. The solr query params are stored in the session,
   # so we only need the +counter+ param here. We also need to know if we are viewing to document as part of search results.
-  def link_to_document(doc, opts={:label=>nil, :counter => nil, :results_view => true})
+  def link_to_document(doc, opts={:label=>nil, :counter => nil})
     opts[:label] ||= blacklight_config.index.show_link.to_sym
     label = render_document_index_label doc, opts
-    link_to label, doc, { :'data-counter' => opts[:counter] }.merge(opts.reject { |k,v| [:label, :counter, :results_view].include? k  })
+    link_to label, doc, { :'data-counter' => opts[:counter] }.merge(opts.reject { |k,v| [:label, :counter].include? k  })
   end
 
   # link_back_to_catalog(:label=>'Back to Search')
