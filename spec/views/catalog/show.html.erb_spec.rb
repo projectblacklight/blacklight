fb2b360cf8f25872ef79b54cfe68f56d66bb13fb
Chris Beer <cabeer@stanford.edu>
2014-01-14 10:44:02 -0800

    add additional catalog/show erb spec tests.

--- spec/views/catalog/show.html.erb_spec.rb
+++ spec/views/catalog/show.html.erb_spec.rb
@@ -25,4 +25,32 @@ describe "catalog/show.html.erb" do
     expect(rendered).to have_selector('div#document[@itemscope]')
     expect(rendered).to have_selector('div#document[@itemtype="some-item-type-uri"]')
   end
+
+  it "should render the show_header and show partials by default" do
+    view.stub(:render_grouped_response?).and_return(false)
+    view.stub(:blacklight_config).and_return(blacklight_config)
+    stub_template "catalog/_show_header_default.html.erb" => "document_header"
+    stub_template "catalog/_show_default.html.erb" => "show_default"
+    
+    assign :document, document
+    render
+
+    expect(rendered).to match /document_header/
+    expect(rendered).to match /show_default/
+  end
+
+
+  it "should use the show.partials parameter to determine the partials to render" do
+    view.stub(:render_grouped_response?).and_return(false)
+    view.stub(:blacklight_config).and_return(blacklight_config)
+    blacklight_config.show.partials = ['a', 'b', 'c']
+    stub_template "catalog/_a_default.html.erb" => "a_partial"
+    stub_template "catalog/_b_default.html.erb" => "b_partial"
+    stub_template "catalog/_c_default.html.erb" => "c_partial"
+    assign :document, document
+    render
+    expect(rendered).to match /a_partial/
+    expect(rendered).to match /b_partial/
+    expect(rendered).to match /c_partial/
+  end
 end
