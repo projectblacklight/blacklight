e3c6ece1c02579cde42406dd8bf5f5c513ee602c
Chris Beer <cabeer@stanford.edu>
2014-02-01 07:40:51 -0800

    add deprecation warnings for unused user methods

--- lib/blacklight/user.rb
+++ lib/blacklight/user.rb
@@ -1,6 +1,9 @@
 # -*- encoding : utf-8 -*-
 module Blacklight::User
 
+  extend Deprecation
+  self.deprecation_horizon = 'blacklight 5.0'
+
   # This gives us an is_blacklight_user method that can be included in
   # the containing applications models. 
   # SEE ALSO:  The /lib/blacklight/engine.rb class for how when this 
@@ -19,10 +22,12 @@ module Blacklight::User
   end
 
   def has_bookmarks?
+    Deprecation.warn(Blacklight::User, "User#has_bookmarks? is deprecated; use User#bookmarks.any? instead")
     bookmarks.any?
   end
     
   def has_searches?
+    Deprecation.warn(Blacklight::User, "User#has_searchs? is deprecated; use User#searches.any? instead")
     searches.any?
   end
     
@@ -41,6 +46,7 @@ module Blacklight::User
   end
     
   def documents_to_bookmark=(docs)
+    Deprecation.warn(Blacklight::User, "User#documents_to_bookmarks= is deprecated and will be removed in Blacklight 5.0")
     docs.reject { |doc| document_is_bookmarked?(doc[:document_id]) }.each do |doc|
       self.bookmarks.create(doc) 
     end
