71c6ec8860e8470e15714212aee3dcc075d0d2f9
Chris Colvard <cjcolvar@indiana.edu>
2014-02-04 22:04:51 -0500

    Group facets into separate blocks based upon group specified on facet field in config

--- spec/views/catalog/_facets.html.erb_spec.rb
+++ spec/views/catalog/_facets.html.erb_spec.rb
@@ -5,34 +5,67 @@ describe "catalog/_facets" do
     @mock_config = Blacklight::Configuration.new
     view.stub(:blacklight_config => @mock_config)
   end
-  it "should not have a header if no facets are displayed" do
-    view.stub(:render_facet_partials => '')
-    render
-    expect(rendered).to_not have_selector('h4')
-  end
 
-  it "should have a header" do
+  describe "facet groups" do
+    describe "default facet group" do
+      it "should not have a header if no facets are displayed" do
+        view.stub(:render_facet_partials => '')
+        render
+        expect(rendered).to_not have_selector('h4')
+      end
 
-      @mock_field_1 = double(:field => 'facet_field_1',
-                       :label => 'label')
-      @mock_display_facet_1 = double(:name => 'facet_field_1', :items => [Blacklight::SolrResponse::Facets::FacetItem.new(:value => 'Value', :hits => 1234)])
-      view.stub(:facet_field_names => [:facet_field_1],
+      it "should have a header" do
+
+        @mock_field_1 = double(:field => 'facet_field_1',
+                       :label => 'label', :group => nil)
+        @mock_display_facet_1 = double(:name => 'facet_field_1', :items => [Blacklight::SolrResponse::Facets::FacetItem.new(:value => 'Value', :hits => 1234)])
+        view.stub(:facet_group_names => [nil], :facet_field_names => [:facet_field_1],
                 :facet_limit_for => 10 )
 
-      @response = double()
-      @response.stub(:facet_by_field_name).with(:facet_field_1) { @mock_display_facet_1 }
+        @response = double()
+        @response.stub(:facet_by_field_name).with(:facet_field_1) { @mock_display_facet_1 }
 
-    view.stub(:render_facet_partials => '')
-    render
-    expect(rendered).to have_selector('h4')
+        view.stub(:render_facet_partials => '')
+        render
+        expect(rendered).to have_selector('h4')
+      end
+    end
+
+    describe "named facet group" do
+      it "should not have a header if no facets are displayed" do
+        view.stub(:render_facet_partials => '')
+        render
+        expect(rendered).to_not have_selector('h4')
+        expect(rendered).to_not have_selector('#facets-group_1')
+      end
+
+      it "should have a header" do
+
+        @mock_field_1 = double(:field => 'facet_field_1',
+                       :label => 'label', :group => 'group_1')
+        @mock_display_facet_1 = double(:name => 'facet_field_1', :items => [Blacklight::SolrResponse::Facets::FacetItem.new(:value => 'Value', :hits => 1234)])
+        view.stub(:facet_group_names => [nil, 'group_1'], 
+                :facet_limit_for => 10 )
+        view.stub(:facet_field_names).with('group_1').and_return([:facet_field_1])
+
+        @response = double()
+        @response.stub(:facet_by_field_name).with(:facet_field_1) { @mock_display_facet_1 }
+
+        view.stub(:render_facet_partials => '')
+        render
+        expect(rendered).to have_selector('h4')
+        expect(rendered).to have_selector('#facets-group_1')
+      end
+
+    end
   end
 
   describe "facet display" do
     before do
       @mock_field_1 = double(:field => 'facet_field_1',
-                       :label => 'label')
+                       :label => 'label', :group => nil)
       @mock_display_facet_1 = double(:name => 'facet_field_1', :items => [Blacklight::SolrResponse::Facets::FacetItem.new(:value => 'Value', :hits => 1234)])
-      view.stub(:facet_field_names => [:facet_field_1],
+      view.stub(:facet_group_names => [nil], :facet_field_names => [:facet_field_1],
                 :facet_limit_for => 10 )
 
       @response = double()
