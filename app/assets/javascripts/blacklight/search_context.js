bc1e80999ebd7f603675fa1d0565f695b410acbc
Chris Beer <cabeer@stanford.edu>
2014-03-10 13:53:11 -0700

    replace our event handler with a customized version of $.rails.handleMethod

--- app/assets/javascripts/blacklight/search_context.js
+++ app/assets/javascripts/blacklight/search_context.js
@@ -1,17 +1,41 @@
 //= require blacklight/core
 (function($) {
   Blacklight.do_search_context_behavior = function() {
-      function track(event) {
-        this.href = this.getAttribute('data-context-href');
-        this.setAttribute('data-method', 'post');
-        if(event.metaKey || event.ctrlKey) {
-          this.setAttribute('target', '_blank');
-        };
-      }
+    $('a[data-context-href]').on('click.search-context', Blacklight.handleSearchContextMethod);
+  };
 
-      $('a[data-context-href]').on('click', track);
-    };  
-Blacklight.onLoad(function() {
-  Blacklight.do_search_context_behavior();  
-});
+  // this is the $.rails.handleMethod with a couple adjustments, described inline:
+  // first, we're attaching this directly to the event handler, so we can check for meta-keys
+  Blacklight.handleSearchContextMethod = function(event) {
+    var link = $(this);
+
+    // instead of using the normal href, we need to use the context href instead
+    var href = link.data('context-href'),
+      method = 'post',
+      target = link.attr('target'),
+      csrfToken = $('meta[name=csrf-token]').attr('content'),
+      csrfParam = $('meta[name=csrf-param]').attr('content'),
+      form = $('<form method="post" action="' + href + '"></form>'),
+      metadataInput = '<input name="_method" value="' + method + '" type="hidden" />';
+
+    // check for meta keys.. if set, we should open in a new tab
+    if(event.metaKey || event.ctrlKey) {
+      target = '_blank';
+    }
+
+    if (csrfParam !== undefined && csrfToken !== undefined) {
+      metadataInput += '<input name="' + csrfParam + '" value="' + csrfToken + '" type="hidden" />';
+    }
+
+    if (target) { form.attr('target', target); }
+
+    form.hide().append(metadataInput).appendTo('body');
+    form.submit();
+
+    return false;
+  };
+
+  Blacklight.onLoad(function() {
+    Blacklight.do_search_context_behavior();
+  });
 })(jQuery);
