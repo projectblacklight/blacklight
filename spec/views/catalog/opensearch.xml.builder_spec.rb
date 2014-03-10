a699dd94cd4f3fad4f5233f945e3881b0ff59fed
Chris Beer <cabeer@stanford.edu>
2014-03-09 19:40:10 -0700

    re-add #asset_url helper (removed in 12fed9ac)
    fixes #810

---
+++ spec/views/catalog/opensearch.xml.builder_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'catalog/opensearch.xml.builder' do
+  it "should render an OpenSearch description document" do
+    render
+    doc = Nokogiri::XML.parse(rendered)
+    doc.remove_namespaces!
+    expect(doc.xpath('/OpenSearchDescription').length).to eq 1
+  end
+end
