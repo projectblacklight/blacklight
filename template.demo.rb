ef57f015d323947b3afb9eb4883873ed69a064e7
Chris Beer <cabeer@stanford.edu>
2014-06-09 11:01:51 +0300

    allow the user to pass in BLACKLIGHT_INSTALL_OPTIONS to configure the blacklight:install generator

--- template.demo.rb
+++ template.demo.rb
@@ -3,13 +3,17 @@ gem "blacklight", ">= 5.3.0"
 run "bundle install"
 
 # run the blacklight install generator
-generate 'blacklight:install', '--devise --marc --jettywrapper'
+options = ENV.fetch("BLACKLIGHT_INSTALL_OPTIONS", '--devise --marc --jettywrapper')
+
+generate 'blacklight:install', options
 
 # run the database migrations
 rake "db:migrate"
 
 # index some data
-rake "jetty:clean"
-rake "jetty:start"
-rake "blacklight:solr:seed"
-rake "jetty:stop"
+if options =~ /jettywrapper/
+  rake "jetty:clean"
+  rake "jetty:start"
+  rake "blacklight:solr:seed"
+  rake "jetty:stop"
+end
