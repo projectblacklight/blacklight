4eb7251d2743829c156d28e061389eda82be24e3
Chris Beer <cabeer@stanford.edu>
2014-11-10 12:05:33 -0800

    Fix default record email fields

--- lib/blacklight/solr/document/email.rb
+++ lib/blacklight/solr/document/email.rb
@@ -8,7 +8,7 @@ module Blacklight::Solr::Document::Email
     body = []
     body << I18n.t('blacklight.email.text.title', :value => semantics[:title].join(" ")) unless semantics[:title].blank?
     body << I18n.t('blacklight.email.text.author', :value => semantics[:author].join(" ")) unless semantics[:author].blank?
-    body << I18n.t('blacklight.email.text.title', :value => semantics[:title].join(" ")) unless semantics[:title].blank?
+    body << I18n.t('blacklight.email.text.format', :value => semantics[:format].join(" ")) unless semantics[:format].blank?
     body << I18n.t('blacklight.email.text.language', :value => semantics[:language].join(" ")) unless semantics[:language].blank?
     return body.join("\n") unless body.empty?
   end
