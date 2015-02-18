projectblacklight/blacklight

commit 221acd9cde1a96a8ef4301d1ceab9d48a8387d89

Rename solr_document_model and solr_response_model to document_model and response_model

lib/blacklight/solr_response.rb
@@ -16,12 +16,12 @@ class Blacklight::SolrResponse < HashWithIndifferentAccess
   include MoreLikeThis
 
   attr_reader :request_params
-  attr_accessor :solr_document_model
+  attr_accessor :document_model
 
   def initialize(data, request_params, options = {})
     super(force_to_utf8(data))
     @request_params = request_params
-    self.solr_document_model = options[:solr_document_model] || SolrDocument
+    self.document_model = options[:document_model] || SolrDocument
   end
 
   def header
@@ -52,7 +52,7 @@ class Blacklight::SolrResponse < HashWithIndifferentAccess
   end
   
   def documents
-    docs.collect{|doc| solr_document_model.new(doc, self) }
+    docs.collect{|doc| document_model.new(doc, self) }
   end
 
   def grouped

app/helpers/blacklight/blacklight_helper_behavior.rb
@@ -272,7 +272,7 @@ module Blacklight::BlacklightHelperBehavior
   #   @options options [Symbol] :tag
   def render_document_heading(*args)
     options = args.extract_options!
-    if args.first.is_a? blacklight_config.solr_document_model
+    if args.first.is_a? blacklight_config.document_model
       document = args.shift
       tag = options[:tag]
     else

lib/blacklight/bookmarks.rb
@@ -71,7 +71,7 @@ module Blacklight::Bookmarks
     if params[:bookmarks]
       @bookmarks = params[:bookmarks]
     else
-      @bookmarks = [{ document_id: params[:id], document_type: blacklight_config.solr_document_model.to_s }]
+      @bookmarks = [{ document_id: params[:id], document_type: blacklight_config.document_model.to_s }]
     end
 
     current_or_guest_user.save! unless current_or_guest_user.persisted?
@@ -96,7 +96,7 @@ module Blacklight::Bookmarks
   # Beware, :id is the Solr document_id, not the actual Bookmark id.
   # idempotent, as DELETE is supposed to be.
   def destroy
-    bookmark = current_or_guest_user.bookmarks.where(document_id: params[:id], document_type: blacklight_config.solr_document_model).first
+    bookmark = current_or_guest_user.bookmarks.where(document_id: params[:id], document_type: blacklight_config.document_model).first
 
     success = bookmark && bookmark.delete && bookmark.destroyed?
 

lib/blacklight/configuration.rb
@@ -13,6 +13,8 @@ module Blacklight
     require 'blacklight/configuration/facet_field'
     require 'blacklight/configuration/sort_field'
     include Fields
+    extend Deprecation
+    self.deprecation_horizon = 'blacklight 6.0'
 
     # Set up Blacklight::Configuration.default_values to contain
     # the basic, required Blacklight fields
@@ -31,8 +33,8 @@ module Blacklight
           # Default values of parameters to send with every search request
           :default_solr_params => {},
           # the model to load solr response documents into; set below in #initialize_default_values
-          :solr_document_model => nil,
-          :solr_response_model => nil,
+          :document_model => nil,
+          :response_model => nil,
           # The solr rqeuest handler to use when requesting only a single document 
           :document_solr_request_handler => 'document',
           # THe path to send single document requests to solr
@@ -148,13 +150,29 @@ module Blacklight
       end
     end
     
-    def solr_document_model
+    def document_model
       super || SolrDocument
     end
+    alias_method :solr_document_model, :document_model
+    # only here to support alias_method
+    def document_model= *args
+      super
+    end
+    alias_method :solr_document_model=, :document_model=
+    deprecation_deprecate :solr_document_model
+    deprecation_deprecate :solr_document_model=
 
-    def solr_response_model
+    def response_model
       super || Blacklight::SolrResponse
     end
+    alias_method :solr_response_model, :response_model
+    # only here to support alias_method
+    def response_model= *args
+      super
+    end
+    alias_method :solr_response_model=, :response_model=
+    deprecation_deprecate :solr_response_model
+    deprecation_deprecate :solr_response_model=
 
     ##
     # DSL helper
@@ -186,7 +204,7 @@ module Blacklight
     end
     
     def default_title_field
-      solr_document_model.unique_key || 'id'
+      document_model.unique_key || 'id'
     end
 
     ##
@@ -257,8 +275,8 @@ module Blacklight
       # too. These model names should not be `#dup`'ed or we might break ActiveModel::Naming.
       def deep_copy
         deep_dup.tap do |copy|
-          copy.solr_response_model = self.solr_response_model
-          copy.solr_document_model = self.solr_document_model
+          copy.response_model = self.response_model
+          copy.document_model = self.document_model
         end
       end
       alias_method :inheritable_copy, :deep_copy

lib/blacklight/request_builders.rb
@@ -86,7 +86,7 @@ module Blacklight
     ##
     # Retrieve the results for a list of document ids
     def solr_document_ids_params(ids = [])
-      solr_documents_by_field_values_params blacklight_config.solr_document_model.unique_key, ids
+      solr_documents_by_field_values_params blacklight_config.document_model.unique_key, ids
     end
 
     ##

lib/blacklight/solr_repository.rb
@@ -43,7 +43,7 @@ module Blacklight
         key = blacklight_config.http_method == :post ? :data : :params
         res = connection.send_and_receive(path, {key=>solr_params.to_hash, method:blacklight_config.http_method})
 
-        solr_response = blacklight_config.solr_response_model.new(res, solr_params, solr_document_model: blacklight_config.solr_document_model)
+        solr_response = blacklight_config.response_model.new(res, solr_params, document_model: blacklight_config.document_model)
 
         Rails.logger.debug("Solr query: #{solr_params.inspect}")
         Rails.logger.debug("Solr response: #{solr_response.inspect}") if defined?(::BLACKLIGHT_VERBOSE_LOGGING) and ::BLACKLIGHT_VERBOSE_LOGGING

spec/lib/blacklight/configuration_spec.rb
@@ -84,14 +84,14 @@ describe "Blacklight::Configuration" do
       expect(@config.facet_fields).to_not include(@mock_facet)
     end
 
-    it "should not dup solr_response_model or solr_document_model" do
-      @config.solr_response_model = Blacklight::SolrResponse
-      @config.solr_document_model = SolrDocument
+    it "should not dup response_model or document_model" do
+      @config.response_model = Blacklight::SolrResponse
+      @config.document_model = SolrDocument
 
       config_copy = @config.inheritable_copy
 
-      expect(config_copy.solr_response_model).to eq Blacklight::SolrResponse
-      expect(config_copy.solr_document_model).to eq SolrDocument
+      expect(config_copy.response_model).to eq Blacklight::SolrResponse
+      expect(config_copy.document_model).to eq SolrDocument
     end
 
     it "should provide cloned copies of mutable data structures" do

commit b68127abb86b747d12eced072cc705f9b8dead25

Fix backwards-compatibility regression in 221acd9

lib/blacklight/solr_response.rb
@@ -21,7 +21,7 @@ class Blacklight::SolrResponse < HashWithIndifferentAccess
   def initialize(data, request_params, options = {})
     super(force_to_utf8(data))
     @request_params = request_params
-    self.document_model = options[:document_model] || SolrDocument
+    self.document_model = options[:solr_document_model] || options[:document_model] || SolrDocument
   end
 
   def header