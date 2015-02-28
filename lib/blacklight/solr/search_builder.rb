3fd2644d462515734b36aabfe1541d414a2462ae
Adam Wead <amsterdamos@gmail.com>
2015-02-28 10:44:10 -0500

    Use RSolr.solr_escape in place of ::escape

--- lib/blacklight/solr/search_builder.rb
+++ lib/blacklight/solr/search_builder.rb
@@ -241,13 +241,13 @@ module Blacklight::Solr
           facet_config.query[value][:fq]
         when (facet_config and facet_config.date)
           # in solr 3.2+, this could be replaced by a !term query
-          "#{prefix}#{facet_field}:#{RSolr.escape(value)}"
+          "#{prefix}#{facet_field}:#{RSolr.solr_escape(value)}"
         when (value.is_a?(DateTime) or value.is_a?(Date) or value.is_a?(Time))
-          "#{prefix}#{facet_field}:#{RSolr.escape(value.to_time.utc.strftime("%Y-%m-%dT%H:%M:%SZ"))}"
+          "#{prefix}#{facet_field}:#{RSolr.solr_escape(value.to_time.utc.strftime("%Y-%m-%dT%H:%M:%SZ"))}"
         when (value.is_a?(TrueClass) or value.is_a?(FalseClass) or value == 'true' or value == 'false'),
              (value.is_a?(Integer) or (value.to_i.to_s == value if value.respond_to? :to_i)),
              (value.is_a?(Float) or (value.to_f.to_s == value if value.respond_to? :to_f))
-          "#{prefix}#{facet_field}:#{RSolr.escape(value.to_s)}"
+          "#{prefix}#{facet_field}:#{RSolr.solr_escape(value.to_s)}"
         when value.is_a?(Range)
           "#{prefix}#{facet_field}:[#{value.first} TO #{value.last}]"
         else
