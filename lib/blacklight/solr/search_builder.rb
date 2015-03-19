0bac3e35b5118a938356dd6fd5118c44f46ec086
Chris Beer <cabeer@stanford.edu>
2015-03-18 19:10:19 -0700

    Mapping using facet_value_to_fq_string should respect blacklight's facet configuration

--- lib/blacklight/solr/search_builder.rb
+++ lib/blacklight/solr/search_builder.rb
@@ -232,6 +232,9 @@ module Blacklight::Solr
     def facet_value_to_fq_string(facet_field, value) 
       facet_config = blacklight_config.facet_fields[facet_field]
 
+      solr_field = facet_config.field if facet_config and not facet_config.query
+      solr_field ||= facet_field
+
       local_params = []
       local_params << "tag=#{facet_config.tag}" if facet_config and facet_config.tag
 
@@ -243,17 +246,17 @@ module Blacklight::Solr
           facet_config.query[value][:fq]
         when (facet_config and facet_config.date)
           # in solr 3.2+, this could be replaced by a !term query
-          "#{prefix}#{facet_field}:#{RSolr.solr_escape(value)}"
+          "#{prefix}#{solr_field}:#{RSolr.solr_escape(value)}"
         when (value.is_a?(DateTime) or value.is_a?(Date) or value.is_a?(Time))
-          "#{prefix}#{facet_field}:#{RSolr.solr_escape(value.to_time.utc.strftime("%Y-%m-%dT%H:%M:%SZ"))}"
+          "#{prefix}#{solr_field}:#{RSolr.solr_escape(value.to_time.utc.strftime("%Y-%m-%dT%H:%M:%SZ"))}"
         when (value.is_a?(TrueClass) or value.is_a?(FalseClass) or value == 'true' or value == 'false'),
              (value.is_a?(Integer) or (value.to_i.to_s == value if value.respond_to? :to_i)),
              (value.is_a?(Float) or (value.to_f.to_s == value if value.respond_to? :to_f))
-          "#{prefix}#{facet_field}:#{RSolr.solr_escape(value.to_s)}"
+          "#{prefix}#{solr_field}:#{RSolr.solr_escape(value.to_s)}"
         when value.is_a?(Range)
-          "#{prefix}#{facet_field}:[#{value.first} TO #{value.last}]"
+          "#{prefix}#{solr_field}:[#{value.first} TO #{value.last}]"
         else
-          "{!raw f=#{facet_field}#{(" " + local_params.join(" ")) unless local_params.empty?}}#{value}"
+          "{!raw f=#{solr_field}#{(" " + local_params.join(" ")) unless local_params.empty?}}#{value}"
       end
     end
     
