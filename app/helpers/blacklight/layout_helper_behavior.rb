3ca747699a9ed20aed8bf5fe4764fb658622dbcb
Jack Reed <pjreed@stanford.edu>
2015-04-02 16:32:40 -0700

    allow document show classes to be configurable and overrideable with a helper

---
+++ app/helpers/blacklight/layout_helper_behavior.rb
@@ -0,0 +1,34 @@
+# Methods added to this helper will be available to all templates in the hosting
+# application
+module Blacklight
+  # A module for useful methods used in layout configuration
+  module LayoutHelperBehavior
+    ##
+    # Classes added to a document's show content div
+    # @return [String]
+    def show_content_classes
+      "#{main_content_classes} show-document"
+    end
+
+    ##
+    # Classes added to a document's sidebar div
+    # @return [String]
+    def show_sidebar_classes
+      sidebar_classes
+    end
+
+    ##
+    # Classes used for sizing the main content of a Blacklight page
+    # @return [String]
+    def main_content_classes
+      'col-md-9 col-sm-8'
+    end
+
+    ##
+    # Classes used for sizing the sidebar content of a Blacklight page
+    # @return [String]
+    def sidebar_classes
+      'col-md-3 col-sm-4'
+    end
+  end
+end
