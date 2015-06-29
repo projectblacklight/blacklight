0f50b065cbd75369bbadb5b00b079db299f1fdff
Justin Coyne <jcoyne@justincoyne.com>
2015-06-29 17:25:17 -0500

    Update deprecation explanation [ci skip]

--- lib/blacklight/solr/search_builder.rb
+++ lib/blacklight/solr/search_builder.rb
@@ -1,5 +1,6 @@
 module Blacklight::Solr
-  # @deprecated
+  # @deprecated In 6.0 you should have this class generated into your application and Blacklight will
+  #             no longer need to provide it.
   class SearchBuilder < Blacklight::SearchBuilder
     include Blacklight::Solr::SearchBuilderBehavior
   end
