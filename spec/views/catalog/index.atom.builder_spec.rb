ad5cdcc52a0772c1d132c4b7542e982fda67c703
Chris Beer <cabeer@stanford.edu>
2015-07-02 10:57:28 -0700

    Update atom view spec to stub solr calls

--- spec/views/catalog/index.atom.builder_spec.rb
+++ spec/views/catalog/index.atom.builder_spec.rb
@@ -1,42 +1,41 @@
 require 'spec_helper'
+require 'rexml/document'
 
 describe "catalog/index" do  
 
   before(:all) do
-    @config = Blacklight::Configuration.new.configure do |config|
-      config.default_solr_params = {
-        :fl => '*',
-        :rows => 10
-      }
-    end
+    @response = Blacklight::SolrResponse.new({ response: { numFound: 30 }}, { start: 10, rows: 10})
     
-    @params = { 'content_format' => 'marc', :f => { :format => ['Book'] }, :page => 2 }
-
-    # run a solr query to get our data
-    c = CatalogController.new
-    c.blacklight_config = @config
-    @response, @document_list = c.search_results(@params, c.search_params_logic)
-
-    # munge the solr response to match test expectations
-    @document_list[1] = SolrDocument.new(@document_list[1].with_indifferent_access.reject! { |k,v| k == "author_display" })
-    @document_list[5] = SolrDocument.new(@document_list[1].with_indifferent_access.reject! { |k,v| k == "marc_display" })
+    @config = CatalogController.blacklight_config
   end
+
+  let(:document_list) do
+    10.times.map { |i| SolrDocument.new(id: i) }
+  end
+
   before(:each) do
     # Not sure what Assigns was doing here ... dhf
     #    assigns[:response] = @rsolr_response
     #    assigns[:document_list] = @document_list
     # not sure why we can't use assigns for 'params', instead this weird way,
     # but okay. 
+    
+    params.merge!( 'content_format' => 'some_format' )
+    @document_list = document_list
+    allow_any_instance_of(SolrDocument).to receive(:export_as_some_format).and_return("")
+    allow(document_list[0]).to receive(:to_semantic_values).and_return(author: ['xyz'])
+    @document_list[1].will_export_as(:some_format, "application/some-format")
 
-    params.merge!( @params )
     allow(view).to receive(:blacklight_config).and_return(@config)
     allow(view).to receive(:search_field_options_for_select).and_return([])
 
     render :template => 'catalog/index', :formats => [:atom] 
+  end
 
+  let(:response_xml) do
     # We need to use rexml to test certain things that have_tag wont' test    
     # note that response is depricated rails 3, use "redered" instead. 
-    @response_xml = REXML::Document.new(rendered)   
+    REXML::Document.new(rendered)   
   end
 
   it "should have contextual information" do
@@ -51,15 +50,15 @@ describe "catalog/index" do
   
   it "should get paging data correctly from response" do
     # Can't use have_tag for namespaced elements, sorry.    
-    expect(@response_xml.elements["/feed/opensearch:totalResults"].text).to eq "30"
-    expect(@response_xml.elements["/feed/opensearch:startIndex"].text).to eq "10"
-    expect(@response_xml.elements["/feed/opensearch:itemsPerPage"].text).to eq "10"        
+    expect(response_xml.elements["/feed/opensearch:totalResults"].text).to eq "30"
+    expect(response_xml.elements["/feed/opensearch:startIndex"].text).to eq "10"
+    expect(response_xml.elements["/feed/opensearch:itemsPerPage"].text).to eq "10"        
   end
   
   it "should include an opensearch Query role=request" do
         
-    expect(@response_xml.elements.to_a("/feed/opensearch:itemsPerPage")).to have(1).item
-    query_el = @response_xml.elements["/feed/opensearch:Query"]
+    expect(response_xml.elements.to_a("/feed/opensearch:itemsPerPage")).to have(1).item
+    query_el = response_xml.elements["/feed/opensearch:Query"]
     expect(query_el).to_not be_nil
     expect(query_el.attributes["role"]).to eq "request"
     expect(query_el.attributes["searchTerms"]).to eq ""
@@ -89,16 +88,16 @@ describe "catalog/index" do
     
     describe "with an author" do
       before do
-        @entry = @response_xml.elements.to_a("/feed/entry")[0]
+        @entry = response_xml.elements.to_a("/feed/entry")[0]
       end
       it "should have author tag" do
-        expect(@entry.elements["author/name"]).not_to be_nil              
+        expect(@entry.elements["author/name"].text).to eq 'xyz'
       end
     end
     
     describe "without an author" do
       before do
-        @entry = @response_xml.elements.to_a("/feed/entry")[1]
+        @entry = response_xml.elements.to_a("/feed/entry")[1]
       end
       it "should not have an author tag" do
         expect(@entry.elements["author/name"]).to be_nil
@@ -108,25 +107,23 @@ describe "catalog/index" do
   
   describe "when content_format is specified" do
     describe "for an entry with content available" do
-      before do
-        @entry = @response_xml.elements.to_a("/feed/entry")[0]
+      let(:entry) do
+        response_xml.elements.to_a("/feed/entry")[1].to_s
       end
       it "should include a link rel tag" do
-        expect(@entry.to_s).to have_selector("link[rel=alternate][type='application/marc']")
+        expect(entry).to have_selector("link[rel=alternate][type='application/some-format']")
       end
       it "should have content embedded" do
-        expect(@entry.to_s).to have_selector("content")
+        expect(entry).to have_selector("content")
       end
     end
     describe "for an entry with NO content available" do
       before do
-        @entry = @response_xml.elements.to_a("/feed/entry")[5]
+        @entry = response_xml.elements.to_a("/feed/entry")[5]
       end
       it "should include content" do
-        expect(@entry.to_s).to_not have_selector("content[type='application/marc']")
+        expect(@entry.to_s).to_not have_selector("content[type='application/some-format']")
       end
     end
   end
 end
-
-
