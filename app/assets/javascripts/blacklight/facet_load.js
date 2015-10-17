6394d13b42b93f51cea0659415c1431906204268
Jack Reed <pjreed@stanford.edu>
2015-10-17 10:39:40 -0700

    Potential stylistic changes

--- app/assets/javascripts/blacklight/facet_load.js
+++ app/assets/javascripts/blacklight/facet_load.js
@@ -1,13 +1,16 @@
 /*global Blacklight */
 
 (function($) {
-  Blacklight.do_resize_facet_labels_and_counts = function() {
+  'use strict';
+  
+  Blacklight.doResizeFacetLabelsAndCounts = function() {
     // adjust width of facet columns to fit their contents
     function longer (a,b){ return b.textContent.length - a.textContent.length; }
 
-    $('ul.facet-values, ul.pivot-facet').map(function(){
+    $('ul.facet-values, ul.pivot-facet').each(function(){
       var longest = $(this).find('span.facet-count').sort(longer).first();
-      var clone = longest.clone().css('visibility','hidden').css('width', 'auto');
+      var clone = longest.clone()
+        .css('visibility','hidden').css('width', 'auto');
       $('body').append(clone);
       $(this).find('.facet-count').first().width(clone.width());
       clone.remove();
@@ -15,6 +18,6 @@
   };
 
   Blacklight.onLoad(function() {
-    Blacklight.do_resize_facet_labels_and_counts();
+    Blacklight.doResizeFacetLabelsAndCounts();
   });
 })(jQuery);
