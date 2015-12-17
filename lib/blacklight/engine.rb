3d6c1ace309b8d914eee614273ee300a91fe2c84
Chris Beer <cabeer@stanford.edu>
2015-12-17 13:37:55 -0800

    Add blacklight initializer to explicitly precompile favicon.ico for compatiblity with sprockets-rails 3.x

--- lib/blacklight/engine.rb
+++ lib/blacklight/engine.rb
@@ -34,6 +34,10 @@ module Blacklight
       end
     end
 
+    initializer "blacklight.assets.precompile" do |app|
+      app.config.assets.precompile += %w(favicon.ico)
+    end
+
     Blacklight::Engine.config.sms_mappings = {
       'Virgin' => 'vmobl.com',
       'AT&T' => 'txt.att.net',
