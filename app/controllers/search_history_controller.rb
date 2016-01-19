d25aef1d8d246903d27c3ab8da86e0ace0702f49
Justin Coyne <jcoyne@justincoyne.com>
2016-01-18 21:36:48 -0600

    Make SearchHistoryController have an AS::Concern
    This will allow plugins to generate modifications to the
    SearchHistoryController, instead of relying on patches during
    initialization as is currently done in blacklight-range_limit

--- app/controllers/search_history_controller.rb
+++ app/controllers/search_history_controller.rb
@@ -1,21 +1,4 @@
 # -*- encoding : utf-8 -*-
 class SearchHistoryController < ApplicationController
-  include Blacklight::Configurable
-
-  copy_blacklight_config_from(CatalogController)
-
-  def index
-    @searches = searches_from_history
-  end
-  
-  
-  #TODO we may want to remove unsaved (those without user_id) items from the database when removed from history
-  def clear
-    if session[:history].clear
-      flash[:notice] = I18n.t('blacklight.search_history.clear.success')
-    else
-      flash[:error] = I18n.t('blacklight.search_history.clear.failure') 
-    end
-    redirect_to :back
-  end
+  include Blacklight::SearchHistory
 end
