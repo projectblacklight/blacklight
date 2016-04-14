2d376e8bb411181a24e4fee09e7a3f6cc47aec2e
Chris Beer <cabeer@stanford.edu>
2016-04-14 13:52:06 -0700

    Use a PerThreadRegistry for the Blacklight connection and configuration

---
+++ lib/blacklight/runtime_registry.rb
@@ -0,0 +1,9 @@
+require 'active_support/per_thread_registry'
+
+module Blacklight
+  class RuntimeRegistry
+    extend ActiveSupport::PerThreadRegistry
+
+    attr_accessor :connection, :connection_config
+  end
+end
