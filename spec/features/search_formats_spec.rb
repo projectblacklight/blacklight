e27eb853d61702de0a8d7aa33a81fd4c98a1fb74
Chris Beer <chris@cbeer.info>
2016-08-24 08:18:34 -0700

    Update rspec configuration with latest default settings

--- spec/features/search_formats_spec.rb
+++ spec/features/search_formats_spec.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 describe "Search Formats" do
-  before do
-    # Get all the fields from Solr, so the #index documents have the MARC field (to trigger appropriate
-    # export formats)
-    CatalogController.blacklight_config.default_solr_params[:fl] = '*'
-  end
-
   it "has an RSS XML response" do
     visit "/catalog.rss?q="
     expect(page).to have_content "Blacklight Search Results"
