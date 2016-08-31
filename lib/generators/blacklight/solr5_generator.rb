0f28a70f2482223fd91390259b27cdb5580512e1
Chad Nelson <chad.nelson@temple.edu>
2016-08-31 10:32:56 -0400

    Generate solr conf and .solr_wrapper.yml
    Add tests for new generators
    
    Rename  .solr_wrapper ile to .solr_wrapper.yml

--- lib/generators/blacklight/solr5_generator.rb
+++ lib/generators/blacklight/solr5_generator.rb
@@ -2,10 +2,17 @@ require 'rails/generators'
 
 module Blacklight
   class Solr5Generator < Rails::Generators::Base
+
+    # Set source_root to grab .solr_wrapper and solr config dir
+    # from the root of the blacklight gem
+    source_root Blacklight.root
+
     desc <<-EOF
       This generator makes the following changes to your application:
        1. Installs solr_wrapper into your application
-       2. Adds rsolr to your Gemfile
+       2. Copies default blacklight solr config directory into your application
+       3. Copies default .solr_wrapper into your application
+       4. Adds rsolr to your Gemfile
     EOF
 
     def install_solrwrapper
@@ -14,6 +21,14 @@ module Blacklight
       end
 
       append_to_file "Rakefile", "\nrequire 'solr_wrapper/rake_task' unless Rails.env.production?\n"
+    end
+
+    def copy_solr_conf
+      directory 'solr', 'solr'
+    end
+
+    def solr_wrapper_config
+      copy_file '.solr_wrapper.yml', '.solr_wrapper.yml'
     end
 
     def add_rsolr_gem
---
+++ spec/integration/generators/blacklight/solr5_generator_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+require 'generators/blacklight/solr5_generator'
+
+describe Blacklight::Solr5Generator do
+  let(:destination) { Dir.mktmpdir }
+
+  describe "#solr_wrapper_config" do
+    let(:generator) { described_class.new }
+    let(:files_to_test) {[
+        File.join("#{destination}", '.solr_wrapper.yml')
+    ]}
+
+    before do
+      generator.destination_root = destination
+      generator.solr_wrapper_config
+
+    end
+
+    after do
+      files_to_test.each { |file| File.delete(file) if File.exist?(file) }
+    end
+
+    it "creates config files" do
+      files_to_test.each do |file|
+        expect(File).to exist(file), "Expected #{file} to exist"
+      end
+    end
+  end
+
+  describe "#copy_solr_conf" do
+    let(:generator) { described_class.new }
+    let(:dirs_to_test) {[
+        File.join("#{destination}", 'solr'),
+        File.join("#{destination}", 'solr/conf')]}
+    let(:files_to_test) {[
+        File.join("#{destination}", 'solr/conf/solrconfig.xml')
+    ]}
+
+    before do
+      generator.destination_root = destination
+      generator.copy_solr_conf
+    end
+
+    after do
+      dirs_to_test.each { |dir| FileUtils.rm_rf(Dir.glob(dir)) if File.directory?(dir) }
+    end
+
+    it "creates solr directory" do
+      dirs_to_test.each do |dir|
+        expect(File).to exist(dir), "Expected #{dir} to exist"
+      end
+    end
+
+    it "copies solr config files" do
+      files_to_test.each do |file|
+        expect(File).to exist(file), "Expected #{file} to exist"
+      end
+    end
+  end
+end
