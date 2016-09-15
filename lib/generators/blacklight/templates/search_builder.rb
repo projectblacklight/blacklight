f64b9c2f642835c6a683a4b926e6bb5e61ba647e
Chris Beer <chris@cbeer.info>
2016-09-15 14:59:38 -0400

    Add example usage to the generated SearchBuilder; fixes #1492

--- lib/generators/blacklight/templates/search_builder.rb
+++ lib/generators/blacklight/templates/search_builder.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
+
+  ##
+  # @example Adding a new step to the processor chain
+  #   self.default_processor_chain += [:add_custom_data_to_query]
+  #
+  #   def add_custom_data_to_query(solr_parameters)
+  #     solr_parameters[:custom] = blacklight_params[:user_value]
+  #   end
 end
