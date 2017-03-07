279241e15c4b409d29b6e9d4d76f2d79f243bc41
Justin Coyne <jcoyne@justincoyne.com>
2017-03-07 11:52:18 -0800

    Pass controller instance to SearchState
    This allows subclasses to access the url_helpers

--- app/helpers/blacklight/render_constraints_helper_behavior.rb
+++ app/helpers/blacklight/render_constraints_helper_behavior.rb
@@ -64,14 +64,14 @@ module Blacklight::RenderConstraintsHelperBehavior
   # @param [Hash] localized_params query parameters
   # @return [String]
   def render_constraints_filters(localized_params = params)
-     return "".html_safe unless localized_params[:f]
-     path = controller.search_state_class.new(localized_params, blacklight_config)
-     content = []
-     localized_params[:f].each_pair do |facet,values|
-       content << render_filter_element(facet, values, path)
-     end
+    return "".html_safe unless localized_params[:f]
+    path = controller.search_state_class.new(localized_params, blacklight_config, controller)
+    content = []
+    localized_params[:f].each_pair do |facet,values|
+      content << render_filter_element(facet, values, path)
+    end
 
-     safe_join(content.flatten, "\n")
+    safe_join(content.flatten, "\n")
   end
 
   ##
