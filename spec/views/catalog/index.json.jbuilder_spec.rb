801589676aa5e2dee14b8482a0b391feb105ea7b
Chris Beer <chris@cbeer.info>
2017-05-04 08:07:25 -0700

    Update JSON API tests

--- spec/views/catalog/index.json.jbuilder_spec.rb
+++ spec/views/catalog/index.json.jbuilder_spec.rb
@@ -6,32 +6,41 @@ RSpec.describe "catalog/index.json" do
   let(:config) { Blacklight::Configuration.new }
   let(:presenter) { Blacklight::JsonPresenter.new(response, facets, config) }
 
-  it "renders index json" do
+  let(:hash) do
+    render template: "catalog/index.json", format: :json
+    JSON.parse(rendered).with_indifferent_access
+  end
+
+  before do
     allow(view).to receive(:blacklight_config).and_return(config)
+    allow(view).to receive(:search_action_path).and_return('http://test.host/some/search/url')
+    allow(view).to receive(:search_facet_path).and_return('http://test.host/some/facet/url')
     allow(presenter).to receive(:pagination_info).and_return({ current_page: 1, next_page: 2,
                                                                prev_page: nil })
     allow(presenter).to receive(:search_facets_as_json).and_return(
-          [{ name: "format", label: "Format",
-             items: [{ value: 'Book', hits: 30, label: 'Book' }] }])
+          [{ 'name' => "format", 'label' => "Format",
+             'items' => [{ 'value' => 'Book', 'hits' => 30, 'label' => 'Book' }] }])
     assign :presenter, presenter
     assign :response, response
-    render template: "catalog/index.json", format: :json
-    hash = JSON.parse(rendered).with_indifferent_access
-    expect(hash).to include(links: hash_including(self: 'http://test.host/', next: 'http://test.host/?page=2', last: 'http://test.host/?page=3'))
-    expect(hash).to include(response: hash_including(facets: [
-      { 
-        'name' => "format", 'label' => "Format",
-        'items' => [
-          { 'value' => 'Book',
-            'hits' => 30,
-            'label' => 'Book' }] 
-      }]))
-    expect(hash).to include(response: hash_including(pages: 
+  end
+
+  it "has pagination links" do
+    expect(hash).to include(links: hash_including(
+      self: 'http://test.host/',
+      next: 'http://test.host/?page=2',
+      last: 'http://test.host/?page=3'))
+  end
+
+  it "has pagination information" do
+    expect(hash).to include(meta: hash_including(pages:
       { 
         'current_page' =>  1,
         'next_page' => 2,
         'prev_page' => nil 
       }))
+  end
+
+  it "includes documents, links, and their attributes" do
     expect(hash).to include(data: [
       {
         id: '123',
@@ -44,5 +53,23 @@ RSpec.describe "catalog/index.json" do
         links: { self: 'http://test.host/catalog/456' }
       },
     ])
+  end
+
+  it "has facet information and links" do
+    expect(hash).to include(:included)
+
+    facets = hash[:included].select { |x| x['type'] == 'facet' }
+    expect(facets).to be_present
+
+    expect(facets.map { |x| x['id'] }).to include 'format'
+
+    format = facets.find { |x| x['id'] == 'format' }
+
+    expect(format['links']).to include self: 'http://test.host/some/facet/url'
+    expect(format['attributes']).to include :items
+
+    format_items = format['attributes']['items'].map { |x| x['attributes'] }
+
+    expect(format_items).to match_array [{value: 'Book', hits: 30, label: 'Book'}]
   end
 end
