a1f3696a6d32d9f6252c492fb4ccd729abf5625a
Chris Beer <cabeer@stanford.edu>
2018-01-08 11:23:55 -0800

    Convert our generated catalog controller configuration to be compatible with Solr 7.2

--- lib/generators/blacklight/templates/catalog_controller.rb
+++ lib/generators/blacklight/templates/catalog_controller.rb
@@ -137,23 +137,18 @@ class <%= controller_name.classify %>Controller < ApplicationController
 
     config.add_search_field('title') do |field|
       # solr_parameters hash are sent to Solr as ordinary url query params.
-      field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
-
-      # :solr_local_parameters will be sent using Solr LocalParams
-      # syntax, as eg {! qf=$title_qf }. This is neccesary to use
-      # Solr parameter de-referencing like $title_qf.
-      # See: http://wiki.apache.org/solr/LocalParams
-      field.solr_local_parameters = {
-        qf: '$title_qf',
-        pf: '$title_pf'
+      field.solr_parameters = {
+        'spellcheck.dictionary': 'title',
+        qf: '${title_qf}',
+        pf: '${title_pf}'
       }
     end
 
     config.add_search_field('author') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
-      field.solr_local_parameters = {
-        qf: '$author_qf',
-        pf: '$author_pf'
+      field.solr_parameters = {
+        'spellcheck.dictionary': 'author',
+        qf: '${author_qf}',
+        pf: '${author_pf}'
       }
     end
 
@@ -161,11 +156,11 @@ class <%= controller_name.classify %>Controller < ApplicationController
     # tests can test it. In this case it's the same as
     # config[:default_solr_parameters][:qt], so isn't actually neccesary.
     config.add_search_field('subject') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
       field.qt = 'search'
-      field.solr_local_parameters = {
-        qf: '$subject_qf',
-        pf: '$subject_pf'
+      field.solr_parameters = {
+        'spellcheck.dictionary': 'subject',
+        qf: '${subject_qf}',
+        pf: '${subject_pf}'
       }
     end
 
