148d91d410edbdd8d97c9f54805ee28eabffbf0a
Chris Beer <cabeer@stanford.edu>
2018-01-18 15:37:04 -0800

    Use Chrome with sandboxing disabled

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,7 +32,7 @@ Capybara.javascript_driver = :headless_chrome
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[headless disable-gpu] }
+    chromeOptions: { args: %w[headless disable-gpu no-sandbox] }
   )
 
   Capybara::Selenium::Driver.new(app,
