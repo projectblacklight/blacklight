89cce01c0aa6b14423e3a1520ede84d3a302a13d
xhero <rwz@xhero.org>
2018-03-21 13:54:07 +0100

    $(document).on('ready') is deprecated from JQuery 1.8

--- app/assets/javascripts/blacklight/core.js
+++ app/assets/javascripts/blacklight/core.js
@@ -9,31 +9,22 @@ Blacklight = function() {
       for(var i = 0; i < buffer.length; i++) {
         buffer[i].call();
       }
-    },
-
-    listeners: function () {
-      var listeners = [];
-      if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
-        // Turbolinks 5
-        if (Turbolinks.BrowserAdapter) {
-          listeners.push('turbolinks:load');
-        } else {
-          // Turbolinks < 5
-          listeners.push('page:load', 'ready');
-        }
-      } else {
-        listeners.push('ready');
-      }
-
-      return listeners.join(' ');
     }
   };
 }();
 
 // turbolinks triggers page:load events on page transition
-// If app isn't using turbolinks, this event will never be triggered, no prob. 
-$(document).on(Blacklight.listeners(), function() {
-  Blacklight.activate();  
-});
-
-
+// If app isn't using turbolinks, this event will never be triggered, no prob.
+// $(document).on('ready') is deprecated in JQuery 1.8
+if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
+  // Turbolinks 5
+  if (Turbolinks.BrowserAdapter) {
+    $(document).on('turbolinks:load', function() { Blacklight.activate(); });
+  } else {
+    // Turbolinks < 5
+    $(document).on('page:load', function() { Blacklight.activate(); });
+    $(document).ready(function() { Blacklight.activate(); });
+  }
+} else {
+  $(document).ready(function() { Blacklight.activate(); });
+}
