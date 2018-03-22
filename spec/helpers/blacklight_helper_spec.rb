2bf25dfe715728942e250bf98bbe4c739265ceba
David Kinzer <dtkinzer@gmail.com>
2018-03-21 20:38:57 -0400

    Do not assume spelling is available.
    Solr is not always the repository so it's not safe to assume that the
    spelling field is available in the response.
    
    This commit short circuits the response when spelling is not available,
    otherwise an exception is thrown when trying to get words from nil.

--- spec/helpers/blacklight_helper_spec.rb
+++ spec/helpers/blacklight_helper_spec.rb
@@ -240,12 +240,16 @@ describe BlacklightHelper do
       response = double(total: 10)
       expect(helper.should_show_spellcheck_suggestions? response).to be false
     end
-    it "onlies show suggestions if there are very few results" do
+    it "only shows suggestions if there are very few results" do
       response = double(total: 4, spelling: double(words: [1]))
       expect(helper.should_show_spellcheck_suggestions? response).to be true
     end
     it "shows suggestions only if there are spelling suggestions available" do
       response = double(total: 4, spelling: double(words: []))
+      expect(helper.should_show_spellcheck_suggestions? response).to be false
+    end
+    it "does not show suggestions if spelling is not available" do
+      response = double(total: 4, spelling: nil)
       expect(helper.should_show_spellcheck_suggestions? response).to be false
     end
   end
