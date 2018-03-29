68affcfee3d28f9824c83be6e2e33bee38b5da2b
Justin Coyne <jcoyne@justincoyne.com>
2018-03-29 13:33:37 -0700

    Remove unused selector: Blacklight.modal.preserveFormSelector (#1761)

--- app/assets/javascripts/blacklight/modal.js
+++ app/assets/javascripts/blacklight/modal.js
@@ -95,7 +95,6 @@ Blacklight.modal.triggerFormSelector  = 'form[data-blacklight-modal~=trigger]';
 // be preserved. MUST be manually prefixed with the modal selector,
 // so they only apply to things inside a modal.
 Blacklight.modal.preserveLinkSelector = Blacklight.modal.modalSelector + ' a[data-blacklight-modal~=preserve]';
-Blacklight.modal.preserveFormSelector = Blacklight.modal.modalSelector + ' form[data-blacklight-modal~=preserve]'
 
 Blacklight.modal.containerSelector    = '[data-blacklight-modal~=container]';
 
@@ -171,7 +170,7 @@ Blacklight.modal.setupModal = function() {
   // Register both trigger and preserve selectors in ONE event handler, combining
   // into one selector with a comma, so if something matches BOTH selectors, it
   // still only gets the event handler called once.
-  $('body').on('click', Blacklight.modal.triggerLinkSelector  + ', ' + Blacklight.modal.preserveLinkSelector,
+  $('body').on('click', Blacklight.modal.triggerLinkSelector + ', ' + Blacklight.modal.preserveLinkSelector,
     Blacklight.modal.modalAjaxLinkClick);
   $('body').on('submit', Blacklight.modal.triggerFormSelector + ', ' + Blacklight.modal.preserveFormSelector,
     Blacklight.modal.modalAjaxFormSubmit);
