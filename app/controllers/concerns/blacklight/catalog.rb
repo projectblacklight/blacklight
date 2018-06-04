456a366d54b0268a1d8125a37a868346d7c31df5
Justin Coyne <jcoyne@justincoyne.com>
2018-06-04 10:45:21 -0500

    Only use helper_method when the view layer is present
    In API mode, the view layer is not available and these trigger
    method_missing errors. This fix is similar to how devise solved this
    problem: https://github.com/plataformatec/devise/pull/3732

--- app/controllers/concerns/blacklight/catalog.rb
+++ app/controllers/concerns/blacklight/catalog.rb
@@ -9,7 +9,9 @@ module Blacklight::Catalog
   # The following code is executed when someone includes blacklight::catalog in their
   # own controller.
   included do
-    helper_method :sms_mappings, :has_search_parameters?
+    if respond_to? :helper_method
+      helper_method :sms_mappings, :has_search_parameters?
+    end
 
     helper Blacklight::Facet
 
