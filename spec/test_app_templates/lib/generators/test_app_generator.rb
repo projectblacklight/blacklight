88de9a471ada2a52a22c0b982f7a2d1f9cde7121
Justin Coyne <jcoyne@justincoyne.com>
2018-06-06 05:12:50 -0700

    remove fix for Rails 4 that is no longer used

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -4,20 +4,12 @@ require 'rails/generators'
 class TestAppGenerator < Rails::Generators::Base
   source_root File.expand_path("../../../../test_app_templates", __FILE__)
 
-  def fix_travis_rails_4 
-    if ENV['TRAVIS']
-      insert_into_file 'app/assets/stylesheets/application.css', :before =>'/*' do      
-        "@charset \"UTF-8\";\n"
-      end
-    end
-  end
-
-  def remove_index 
+  def remove_index
     remove_file "public/index.html"
   end
 
   def run_blacklight_generator
-    say_status("warning", "GENERATING BL", :yellow)       
+    say_status("warning", "GENERATING BL", :yellow)
 
     Bundler.with_clean_env do
       run "bundle install"
@@ -27,7 +19,7 @@ class TestAppGenerator < Rails::Generators::Base
   end
 
   def run_test_support_generator
-    say_status("warning", "GENERATING test_support", :yellow)       
+    say_status("warning", "GENERATING test_support", :yellow)
 
     generate 'blacklight:test_support'
   end
