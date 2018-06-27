d7ffac0c9a5d0267045f551a4e06767549b24c78
Justin Coyne <jcoyne@justincoyne.com>
2018-06-27 10:40:05 -0500

    Extract reflect_fields method to the repository
    This allows the repository to implement reflect_fields and allows us to
    remove a conditional.

--- lib/blacklight/solr/repository.rb
+++ lib/blacklight/solr/repository.rb
@@ -30,6 +30,13 @@ module Blacklight::Solr
     end
 
     ##
+    # Gets a list of available fields
+    # @return [Hash]
+    def reflect_fields
+      send_and_receive('admin/luke', params: { fl: '*', 'json.nl' => 'map' })['fields']
+    end
+
+    ##
     # Execute a solr query
     # TODO: Make this private after we have a way to abstract admin/luke and ping
     # @see [RSolr::Client#send_and_receive]
