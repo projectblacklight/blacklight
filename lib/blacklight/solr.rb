f16545f08b55a29eddf7eb478a60cd691db8997b
Justin Coyne <jcoyne@justincoyne.com>
2019-09-11 10:54:25 -0500

    Move all the solr modules under lib so zeitwerk can find them

--- lib/blacklight/solr.rb
+++ lib/blacklight/solr.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 module Blacklight
   module Solr
+    autoload :Document, 'blacklight/solr/document'
+    autoload :FacetPaginator, 'blacklight/solr/facet_paginator'
     autoload :Repository, 'blacklight/solr/repository'
     autoload :Request, 'blacklight/solr/request'
     autoload :Response, 'blacklight/solr/response'
