87dfc83181811a389a068d4a167eab602b35977b
Justin Coyne <jcoyne@justincoyne.com>
2019-10-04 10:42:59 -0500

    Pass the request object as a parameter rather than relying on an instance variable
    This makes it easier to reason about dependencies

--- app/controllers/concerns/blacklight/facet.rb
+++ app/controllers/concerns/blacklight/facet.rb
@@ -18,8 +18,14 @@ module Blacklight
 
     # @param fields [Array<String>] a list of facet field names
     # @return [Array<Solr::Response::Facets::FacetField>]
-    def facets_from_request(fields = facet_field_names)
-      fields.map { |field| facet_by_field_name(field) }.compact
+    def facets_from_request(fields = facet_field_names, response = nil)
+      unless response
+        Deprecation.warn(self, 'Calling facets_from_request without passing the ' \
+          'second argument (response) is deprecated and will be removed in Blacklight ' \
+          '8.0.0')
+        response = @response
+      end
+      fields.map { |field| facet_by_field_name(field, response) }.compact
     end
 
     def facet_group_names
@@ -27,13 +33,19 @@ module Blacklight
     end
 
     # Get a FacetField object from the @response
-    def facet_by_field_name(field_or_field_name)
+    def facet_by_field_name(field_or_field_name, response = nil)
+      unless response
+        Deprecation.warn(self, 'Calling facet_by_field_name without passing the ' \
+          'second argument (response) is deprecated and will be removed in Blacklight ' \
+          '8.0.0')
+        response = @response
+      end
       case field_or_field_name
       when String, Symbol
         facet_field = facet_configuration_for_field(field_or_field_name)
-        @response.aggregations[facet_field.field]
+        response.aggregations[facet_field.field]
       when Blacklight::Configuration::FacetField
-        @response.aggregations[field_or_field_name.field]
+        response.aggregations[field_or_field_name.field]
       else
         # is this really a useful case?
         field_or_field_name
