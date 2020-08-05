7cdd8a182f4e0d815026c9d9ceeee273df0a419a
k8sewell <kait@notch8.com>
2020-08-05 11:47:23 -0700

    Fix rubocop offenses

--- spec/views/catalog/_search_form.erb_spec.rb
+++ spec/views/catalog/_search_form.erb_spec.rb
@@ -3,7 +3,6 @@
 # spec for search form input view
 
 RSpec.describe "/catalog/_search_form.html.erb" do
-
   it "renders aria label for search input" do
     stub_template "_search_form.html.erb" => "search_form"
     render
