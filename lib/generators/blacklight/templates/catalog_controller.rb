174e0a821ed9edc89577ff2ac1bece7752f2a846
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2020-10-06 16:25:55 -0400

    updates sort field config in catalog controller template

--- lib/generators/blacklight/templates/catalog_controller.rb
+++ lib/generators/blacklight/templates/catalog_controller.rb
@@ -184,10 +184,11 @@ class <%= controller_name.classify %>Controller < ApplicationController
     # "sort results by" select (pulldown)
     # label in pulldown is followed by the name of the Solr field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
-    # except in the relevancy case).
-    config.add_sort_field 'score desc, pub_date_si desc, title_si asc', label: 'relevance'
-    config.add_sort_field 'pub_date_si desc, title_si asc', label: 'year'
-    config.add_sort_field 'author_si asc, title_si asc', label: 'author'
+    # except in the relevancy case). Add the sort: option to configure a
+    # custom Blacklight url parameter value separate from the Solr sort fields.
+    config.add_sort_field 'relevance', sort: 'score desc, pub_date_si desc, title_si asc', label: 'relevance'
+    config.add_sort_field 'year-desc', sort: 'pub_date_si desc, title_si asc', label: 'year'
+    config.add_sort_field 'author', sort: 'author_si asc, title_si asc', label: 'author'
     config.add_sort_field 'title_si asc, pub_date_si desc', label: 'title'
 
     # If there are more than this many search results, no spelling ("did you
