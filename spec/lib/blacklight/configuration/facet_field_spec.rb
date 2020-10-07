86e12b889b77a73eed30f3eec498238c189c08de
Chris Beer <cabeer@stanford.edu>
2020-10-07 11:56:50 -0700

    Allow custom presenters to be set for facet fields

--- spec/lib/blacklight/configuration/facet_field_spec.rb
+++ spec/lib/blacklight/configuration/facet_field_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe Blacklight::Configuration::FacetField do
       expect(subject.link_to_facet).to eq true
     end
   end
+
+  describe '#normalize!' do
+    it 'preserves existing properties' do
+      expected = double
+      subject.presenter = expected
+
+      subject.normalize!
+
+      expect(subject.presenter).to eq expected
+    end
+
+    it 'adds a default presenter' do
+      subject.normalize!
+
+      expect(subject.presenter).to eq Blacklight::FacetFieldPresenter
+    end
+  end
 end
