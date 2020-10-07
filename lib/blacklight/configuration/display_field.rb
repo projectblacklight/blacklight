b0787f3c732060fa8a0896aee5dacb36920dae2c
Chris Beer <cabeer@stanford.edu>
2020-10-07 07:24:31 -0700

    Extract explicitly defined classes for index + show field configs, just to attach some yard docs to them

---
+++ lib/blacklight/configuration/display_field.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+module Blacklight
+  class Configuration::DisplayField < Blacklight::Configuration::Field
+    ##
+    # The following is a non-exhaustive list of display field config parameters that are used
+    # by Blacklight directly. Application-specific code or plugins may add or replace
+    # the parameters and behaviors specified below.
+    #
+
+    ##
+    # Data parameters:
+    # @!attribute values
+    #  @return [Proc]
+    # @!attribute accessor
+    #  @return [Boolean,Symbol]
+    # @!attribute highlight
+    #  @return [Boolean]
+    # @!attribute default
+    #  @return [Object]
+    # @!attribute solr_params
+    #  @return [Hash]
+    # @!attribute include_in_request
+    #  @return [Boolean]
+
+    ##
+    # Rendering:
+    # @!attribute presenter
+    #   @return [Blacklight::FacetFieldPresenter]
+    # @!attribute component
+    #   @return [Blacklight::FacetFieldListComponent]
+
+    ##
+    # Default rendering pipeline:
+    # @!attribute link_to_facet
+    #   @return [Boolean]
+    # @!attribute link_to_search
+    #   @deprecated use link_to_facet instead.
+    #   @return [Boolean]
+    # @!attribute itemprop
+    #   @return [String]
+    # @!attribute separator_options
+    #   @return [Hash]
+  end
+end
---
+++ lib/blacklight/configuration/show_field.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+module Blacklight
+  class Configuration::ShowField < Blacklight::Configuration::DisplayField
+  end
+end
