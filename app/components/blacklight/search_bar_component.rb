b302bc3501a0ea3eadf02860df36884a00e012fe
Chris Beer <cabeer@stanford.edu>
2020-11-01 08:07:16 -0800

    Open up Blacklight::SearchBarComponent for more customization

--- app/components/blacklight/search_bar_component.rb
+++ app/components/blacklight/search_bar_component.rb
@@ -2,10 +2,18 @@
 
 module Blacklight
   class SearchBarComponent < ::ViewComponent::Base
+    with_content_areas :append, :prepend
+
     # rubocop:disable Metrics/ParameterLists
-    def initialize(url:, params:, classes: ['search-query-form'], presenter: nil, prefix: '', method: 'GET', q: nil, search_field: nil, search_fields: [], autocomplete_path: nil, autofocus: nil)
+    def initialize(
+      url:, params:, classes: ['search-query-form'], presenter: nil,
+      prefix: '', method: 'GET', q: nil, query_param: :q,
+      search_field: nil, search_fields: [], autocomplete_path: nil,
+      autofocus: nil, i18n: { scope: 'blacklight.search.form' }
+    )
       @url = url
       @q = q || params[:q]
+      @query_param = query_param
       @search_field = search_field || params[:search_field]
       @params = params.except(:q, :search_field, :utf8, :page)
       @prefix = prefix
@@ -15,6 +23,7 @@ module Blacklight
       @autocomplete_path = autocomplete_path
       @autofocus = autofocus
       @search_fields = search_fields
+      @i18n = i18n
     end
     # rubocop:enable Metrics/ParameterLists
 
@@ -46,6 +55,10 @@ module Blacklight
       Deprecation.silence(Blacklight::HashAsHiddenFieldsHelperBehavior) do
         @view_context.render_hash_as_hidden_fields(*args)
       end
+    end
+
+    def scoped_t(key, **args)
+      t(key, default: t(key, scope: 'blacklight.search.form'), **@i18n, **args)
     end
   end
 end
