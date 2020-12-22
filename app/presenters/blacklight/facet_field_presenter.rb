45071982ad589583cd07f23932b5aef37733daae
Justin Coyne <jcoyne@justincoyne.com>
2020-12-22 00:30:11 -0600

    Remove deprecated methods facet_limit_for and facet_paginator

--- app/presenters/blacklight/facet_field_presenter.rb
+++ app/presenters/blacklight/facet_field_presenter.rb
@@ -39,9 +39,43 @@ module Blacklight
     def paginator
       return unless display_facet
 
-      Deprecation.silence(Blacklight::Facet) do
-        @paginator ||= view_context.facet_paginator(facet_field, display_facet)
+      @paginator ||= begin
+        blacklight_config.facet_paginator_class.new(
+          display_facet.items,
+          sort: display_facet.sort,
+          offset: display_facet.offset,
+          prefix: display_facet.prefix,
+          limit: facet_limit
+        )
       end
     end
+
+    DEFAULT_FACET_LIMIT = 10
+
+    # Look up facet limit for given facet_field. Will look at config, and
+    # if config is 'true' will look up from Solr @response if available. If
+    # no limit is available, returns nil. Used from #add_facetting_to_solr
+    # to supply f.fieldname.facet.limit values in solr request (no @response
+    # available), and used in display (with @response available) to create
+    # a facet paginator with the right limit.
+    def facet_limit
+      return unless facet_field.limit
+
+      if @display_facet
+        limit = @display_facet.limit
+
+        if limit.nil? # we didn't get or a set a limit, so infer one.
+          facet_field.limit if facet_field.limit != true
+        elsif limit == -1 # limit -1 is solr-speak for unlimited
+          nil
+        else
+          limit.to_i - 1 # we added 1 to find out if we needed to paginate
+        end
+      else
+        facet_field.limit == true ? DEFAULT_FACET_LIMIT : facet.limit
+      end
+    end
+
+    delegate :blacklight_config, to: :search_state
   end
 end
--- spec/presenters/blacklight/facet_field_presenter_spec.rb
+++ spec/presenters/blacklight/facet_field_presenter_spec.rb
@@ -66,7 +66,9 @@ RSpec.describe Blacklight::FacetFieldPresenter, type: :presenter do
     let(:paginator) { Blacklight::FacetPaginator.new([{}, {}], limit: 1) }
 
     before do
-      allow(view_context).to receive(:facet_paginator).and_return(paginator)
+      # rubocop:disable RSpec/SubjectStub
+      allow(presenter).to receive(:paginator).and_return(paginator)
+      # rubocop:enable RSpec/SubjectStub
     end
 
     context 'with no additional data' do
@@ -97,6 +99,83 @@ RSpec.describe Blacklight::FacetFieldPresenter, type: :presenter do
     it 'return a paginator for the facet data' do
       expect(paginator.current_page).to eq 1
       expect(paginator.total_count).to eq 0
+    end
+  end
+
+  describe "#facet_limit" do
+    subject(:facet_limit) { presenter.facet_limit }
+
+    let(:search_state) { Blacklight::SearchState.new({}, blacklight_config, controller) }
+    let(:blacklight_config) { CatalogController.blacklight_config }
+    let(:facet_field) { blacklight_config.facet_fields['subject_ssim'] }
+    let(:display_facet) do
+      instance_double(Blacklight::Solr::Response::Facets::FacetField, items: items, limit: nil, sort: :index, offset: 0, prefix: nil)
+    end
+    # let(:facet_field) { Blacklight::Configuration::FacetField.new(key: 'key') }
+    # let(:display_facet) do
+    #   instance_double(Blacklight::Solr::Response::Facets::FacetField, items: items, sort: :index, offset: 0, prefix: nil)
+    # end
+
+    it "returns specified value for facet_field specified" do
+      expect(facet_limit).to eq facet_field.limit
+    end
+
+    describe "for 'true' configured values" do
+      let(:blacklight_config) do
+        Blacklight::Configuration.new do |config|
+          config.add_facet_field "language_facet", limit: true
+        end
+      end
+      let(:facet_field) { blacklight_config.facet_fields['language_facet'] }
+
+      context 'when the limit in the response is nil' do
+        let(:display_facet) do
+          instance_double(Blacklight::Solr::Response::Facets::FacetField, items: items, limit: nil, sort: :index, offset: 0, prefix: nil)
+        end
+
+        it "uses the config setting" do
+          blacklight_config.facet_fields['language_facet'].limit = 10
+          expect(facet_limit).to eq 10
+        end
+      end
+
+      context 'when the limit in the response is provided' do
+        let(:display_facet) do
+          instance_double(Blacklight::Solr::Response::Facets::FacetField, items: items, limit: 16, sort: :index, offset: 0, prefix: nil)
+        end
+
+        it "gets the limit from the facet field in @response" do
+          expect(facet_limit).to eq 15
+        end
+      end
+
+      context 'when not in the display_facet' do
+        let(:display_facet) { nil }
+
+        it "defaults to 10" do
+          expect(facet_limit).to eq 10
+        end
+      end
+    end
+
+    context 'for facet fields with a key that is different from the field name' do
+      before do
+        allow(controller).to receive(:blacklight_config).and_return(blacklight_config)
+      end
+
+      let(:blacklight_config) do
+        Blacklight::Configuration.new do |config|
+          config.add_facet_field 'some_key', field: 'x', limit: true
+        end
+      end
+      let(:facet_field) { blacklight_config.facet_fields['some_key'] }
+      let(:display_facet) do
+        instance_double(Blacklight::Solr::Response::Facets::FacetField, items: items, limit: 16, sort: :index, offset: 0, prefix: nil)
+      end
+
+      it 'gets the limit from the facet field in the @response' do
+        expect(facet_limit).to eq 15
+      end
     end
   end
 end
