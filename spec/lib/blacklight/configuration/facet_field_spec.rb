72b940a2bdccbc1ca4531707d75857b970901145
Justin Coyne <jcoyne@justincoyne.com>
2020-12-23 21:29:55 -0600

    Remove deprecated facet config option link_to_search

--- spec/lib/blacklight/configuration/facet_field_spec.rb
+++ spec/lib/blacklight/configuration/facet_field_spec.rb
@@ -1,16 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe Blacklight::Configuration::FacetField do
-  describe 'link_to_search' do
-    subject { described_class.new(link_to_search: true) }
-
-    it 'is deprecated' do
-      expect(Deprecation).to receive(:warn)
-      expect(subject.normalize!)
-      expect(subject.link_to_facet).to eq true
-    end
-  end
-
   describe '#normalize!' do
     it 'preserves existing properties' do
       expected = double
