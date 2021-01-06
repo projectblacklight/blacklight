4c4eae5f1a20a07153e4d08a2cbd18202f7d9679
Justin Coyne <jcoyne@justincoyne.com>
2021-01-06 15:51:41 -0600

    Extract a HiddenSearchStateComponent
    This is useful for creating custom components that refine a search with some already selected values

--- app/helpers/blacklight/hash_as_hidden_fields_helper_behavior.rb
+++ app/helpers/blacklight/hash_as_hidden_fields_helper_behavior.rb
@@ -21,43 +21,7 @@ module Blacklight::HashAsHiddenFieldsHelperBehavior
   # @param [Hash] hash
   # @return [String]
   def render_hash_as_hidden_fields(hash)
-    hidden_fields = []
-    flatten_hash(hash).each do |name, value|
-      value = Array.wrap(value)
-      value.each do |v|
-        hidden_fields << hidden_field_tag(name, v.to_s, id: nil)
-      end
-    end
-
-    safe_join(hidden_fields, "\n")
+    render Blacklight::HiddenSearchStateComponent.new(params: hash)
   end
-  deprecation_deprecate render_hash_as_hidden_fields: 'Moving to a private method of Blacklight::SearchBarComponent'
-
-  private
-
-  def flatten_hash(hash = params, ancestor_names = [])
-    flat_hash = {}
-    hash.each do |k, v|
-      names = Array.new(ancestor_names)
-      names << k
-      if v.is_a?(Hash)
-        flat_hash.merge!(flatten_hash(v, names))
-      else
-        key = flat_hash_key(names)
-        key += "[]" if v.is_a?(Array)
-        flat_hash[key] = v
-      end
-    end
-
-    flat_hash
-  end
-
-  def flat_hash_key(names)
-    names = Array.new(names)
-    name = names.shift.to_s.dup
-    names.each do |n|
-      name << "[#{n}]"
-    end
-    name
-  end
+  deprecation_deprecate render_hash_as_hidden_fields: 'Use Blacklight::HiddenSearchStateComponent instead'
 end
