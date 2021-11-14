5d800eee875e4ced0d9452bbfba6f1c95043bab8
Chris Beer <chris@cbeer.info>
2021-11-14 07:19:41 -0800

    Remove deprecation thumbnail presenter behavior

--- app/components/blacklight/document/thumbnail_component.rb
+++ app/components/blacklight/document/thumbnail_component.rb
@@ -20,14 +20,6 @@ module Blacklight
         presenter.thumbnail.exists?
       end
 
-      def use_thumbnail_tag_behavior?
-        !presenter.thumbnail.instance_of?(Blacklight::ThumbnailPresenter)
-      end
-
-      def warn_about_deprecated_behavior
-        Deprecation.warn(Blacklight::Document::ThumbnailComponent, 'Detected as custom thumbnail presenter; make sure it has a #render method that returns just the thumbnail image tag')
-      end
-
       def presenter
         @presenter ||= helpers.document_presenter(@document)
       end
