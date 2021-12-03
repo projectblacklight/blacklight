effb5759b84d81d9fdb706fa49a7319e54cbcd26
Justin Coyne <jcoyne@justincoyne.com>
2021-12-03 17:03:59 -0600

    Find the modal element if it's not passed as an argument
    Fixes a problem with Blacklight 5

--- app/assets/javascripts/blacklight/blacklight.js
+++ app/assets/javascripts/blacklight/blacklight.js
@@ -381,7 +381,7 @@
                 this.type + ' ' + this.url + "\n" + jqXHR.status + ': ' + errorThrown +
                 '</pre></div>';
       $(Blacklight.modal.modalSelector).find('.modal-content').html(contents);
-      $(Blacklight.modal.modalSelector).modal('show');
+      Blacklight.modal.show();
     };
 
     Blacklight.modal.receiveAjax = function (contents) {
@@ -402,7 +402,7 @@
         // if they did preventDefault, don't show the dialog
         if (e.isDefaultPrevented()) return;
 
-        $(Blacklight.modal.modalSelector).modal('show');
+        Blacklight.modal.show();
     };
 
 
@@ -463,12 +463,28 @@
       if ($(event.target).find(Blacklight.modal.modalCloseSelector).length) {
         var modalFlashes = $(this).find('.flash_messages');
 
-        $(event.target).modal('hide');
+        Blacklight.modal.hide(event.target);
         event.preventDefault();
 
         var mainFlashes = $('#main-flashes');
         mainFlashes.append(modalFlashes);
         modalFlashes.fadeIn(500);
+      }
+    };
+
+    Blacklight.modal.hide = function(el) {
+      if (bootstrap.Modal.VERSION >= "5") {
+        bootstrap.Modal.getOrCreateInstance(el || document.querySelector(Blacklight.modal.modalSelector)).hide();
+      } else {
+        $(el || Blacklight.modal.modalSelector).modal('hide');
+      }
+    };
+
+    Blacklight.modal.show = function(el) {
+      if (bootstrap.Modal.VERSION >= "5") {
+        bootstrap.Modal.getOrCreateInstance(el || document.querySelector(Blacklight.modal.modalSelector)).show();
+      } else {
+        $(el || Blacklight.modal.modalSelector).modal('show');
       }
     };
 
