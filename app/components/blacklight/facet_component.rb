a8c8031e804f394ea56b5a4150eaa76a270fd8de
Chris Beer <chris@cbeer.info>
2021-12-04 08:54:22 -0800

    Allow the FacetComponent to receive either the facet response from solr or (preferably) the facet config object

--- app/components/blacklight/facet_component.rb
+++ app/components/blacklight/facet_component.rb
@@ -6,15 +6,23 @@ module Blacklight
   # solr field used for faceting. This renders no UI of it's own, but renders
   # the component that is configured for the facet.
   class FacetComponent < ViewComponent::Base
-    with_collection_parameter :display_facet
+    with_collection_parameter :display_facet_or_field_config
 
     # @param [Blacklight::Solr::Response::Facets::FacetField] display_facet
     # @param [Blacklight::Configuration] blacklight_config
     # @param [Boolean] layout
-    def initialize(display_facet:, blacklight_config:, layout: true)
-      @display_facet = display_facet
-      @field_config = blacklight_config.facet_configuration_for_field(@display_facet.name)
-      @layout = layout
+    def initialize(display_facet_or_field_config: nil, display_facet: nil, field_config: nil, response: nil, blacklight_config: nil, **component_args)
+      if display_facet_or_field_config.is_a?(Blacklight::Configuration::Field) || field_config
+        @field_config = display_facet_or_field_config || field_config
+        @display_facet = display_facet || (response && response.aggregations[@field_config.field])
+      else
+        @display_facet = display_facet || display_facet_or_field_config
+        @field_config = field_config || blacklight_config&.facet_configuration_for_field(@display_facet.name)
+      end
+
+      raise ArgumentError, 'You must provide one of: a) display_facet and blacklight_config, or b) field_config and response' unless @display_facet && @field_config
+
+      @component_args = component_args
     end
 
     def render?
@@ -25,7 +33,7 @@ module Blacklight
       render(
         @field_config.component.new(
           facet_field: helpers.facet_field_presenter(@field_config, @display_facet),
-          layout: @layout
+          **@component_args
         )
       )
     end
