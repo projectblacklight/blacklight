62646d220f79d3b71966d2108a82161da08695cc
Justin Coyne <jcoyne@justincoyne.com>
2021-12-07 15:33:15 -0600

    Update the asset generator for Rails 7

--- lib/generators/blacklight/assets_generator.rb
+++ lib/generators/blacklight/assets_generator.rb
@@ -8,50 +8,56 @@ module Blacklight
 
     # This could be skipped if you want to use webpacker
     def add_javascript_dependencies
-      gem 'bootstrap', options[:'bootstrap-version']
-      gem 'jquery-rails' # This is not a default in Rails 5.1+ or 7.
+      gem 'bootstrap', options[:'bootstrap-version'] # in rails 7, only for stylesheets
     end
 
     ##
     # Remove the empty generated app/assets/images directory. Without doing this,
     # the default Sprockets 4 manifest will raise an exception.
-    def appease_sprockets4
-      return if !defined?(Sprockets::VERSION) || Sprockets::VERSION < '4'
+    # def appease_sprockets4
+    #   return if !defined?(Sprockets::VERSION) || Sprockets::VERSION < '4'
+    #
+    #   append_to_file 'app/assets/config/manifest.js', "\n//= link application.js"
+    #   empty_directory 'app/assets/images'
+    # end
 
-      append_to_file 'app/assets/config/manifest.js', "\n//= link application.js"
-      empty_directory 'app/assets/images'
-    end
-
-    def assets
+    def assets # rubocop:disable Metrics/MethodLength
       copy_file "blacklight.scss", "app/assets/stylesheets/blacklight.scss"
 
       # Ensure this method is idempotent
       return if has_blacklight_assets?
 
       if Rails.version > '7'
-        create_file 'app/assets/javascripts/application.js' do
+        gem "sassc-rails", "~> 2.1"
+        append_to_file 'config/importmap.rb' do
           <<~CONTENT
-            //= require turbo
+            pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.js"
+            pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.0/lib/index.js"
+            pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
+          CONTENT
+        end
+
+        append_to_file 'app/javascripts/application.js' do
+          <<~CONTENT
+            import "jquery"
+            import bootstrap from "bootstrap"
+            window.bootstrap = bootstrap // Required for Blacklight 7 so it can manage the modals
           CONTENT
         end
       else
+        gem 'jquery-rails'
         create_file 'app/assets/javascripts/application.js' do
           <<~CONTENT
             //= require turbolinks
             //= require rails-ujs
+            //= require jquery3
+
+            // Required by Blacklight
+            //= require popper
+            //= require bootstrap
+            //= require blacklight/blacklight
           CONTENT
         end
-      end
-
-      append_to_file 'app/assets/javascripts/application.js' do
-        <<~CONTENT
-          //= require jquery3
-
-          // Required by Blacklight
-          //= require popper
-          //= require bootstrap
-          //= require blacklight/blacklight
-        CONTENT
       end
     end
 
