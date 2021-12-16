45b2474b73674c70a9ae0ace8d88c2c98e5a094c
Justin Coyne <jcoyne@justincoyne.com>
2021-12-16 12:00:40 -0600

    Fix view spec for jbuilder
    This allows this test to work in Rails 7

--- spec/views/catalog/facet.json.jbuilder_spec.rb
+++ spec/views/catalog/facet.json.jbuilder_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe "catalog/facet.json", api: true do
   it "renders facet json" do
     assign :pagination, items: [{ value: 'Book' }]
-    render template: "catalog/facet.json", format: :json
+    render template: "catalog/facet", formats: [:json]
     hash = JSON.parse(rendered)
     expect(hash).to eq('response' => { 'facets' => { 'items' => [{ 'value' => 'Book' }] } })
   end
