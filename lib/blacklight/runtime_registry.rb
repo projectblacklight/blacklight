f51151e920bb5efbadd1a2af87dfedf3feab3b54
Chris Beer <chris@cbeer.info>
2022-02-28 16:56:14 -0800

    Use thread_mattr_accessor instead of deprecated ActiveSupport::PerThreadRegistry
    thread_mattr_accessor was added back in Rails 5.

--- lib/blacklight/runtime_registry.rb
+++ lib/blacklight/runtime_registry.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
-require 'active_support/per_thread_registry'
-
 module Blacklight
   class RuntimeRegistry
-    extend ActiveSupport::PerThreadRegistry
-
-    attr_accessor :connection, :connection_config
+    thread_mattr_accessor :connection, :connection_config
   end
 end
