61b62060f34c242d847b28c29b4acb0101b5346a
Benjamin Armintor <armintor@gmail.com>
2022-05-19 09:32:09 -0400

    use raw svg templates for icon components - cf #2723

--- app/components/blacklight/icons/search_component.rb
+++ app/components/blacklight/icons/search_component.rb
@@ -3,18 +3,8 @@
 module Blacklight
   module Icons
     # This is the magnifing glass icon for the search button.
-    # You can override the default svg by setting:
-    #   Blacklight::Icons::SearchComponent.svg = '<svg>your SVG here</svg>'
-    class SearchComponent < ::ViewComponent::Base
-      def call
-        svg.html_safe # rubocop:disable Rails/OutputSafety
-      end
-
-      class_attribute :svg, default: <<~SVG
-        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" aria-hidden="true" width="24" height="24" viewBox="0 0 24 24">
-          <path fill="none" d="M0 0h24v24H0V0z"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
-        </svg>
-      SVG
-    end
+    # You can override the default svg adding a raw SVG template in your application at
+    # app/components/blacklight/icons/search_component.svg.raw
+    class SearchComponent < Blacklight::Component; end
   end
 end
