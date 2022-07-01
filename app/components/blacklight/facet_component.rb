54cc822feed22c2b1609ce1a482c85a02be7d1c2
Chris Beer <chris@cbeer.info>
2022-07-01 08:15:24 -0700

    Backfill tests for the Blacklight::FacetComponent

--- app/components/blacklight/facet_component.rb
+++ app/components/blacklight/facet_component.rb
@@ -11,12 +11,19 @@ module Blacklight
     # @param [Blacklight::Solr::Response::Facets::FacetField] display_facet
     # @param [Blacklight::Configuration] blacklight_config
     # @param [Boolean] layout
+    # rubocop:disable Metrics/CyclomaticComplexity
     def initialize(display_facet_or_field_config: nil, display_facet: nil, field_config: nil, response: nil, blacklight_config: nil, **component_args)
-      if display_facet_or_field_config.is_a?(Blacklight::Configuration::Field) || field_config
+      if display_facet_or_field_config.is_a? Blacklight::FacetFieldPresenter
+        @facet_field_presenter = display_facet_or_field_config
+        @field_config = @facet_field_presenter.facet_field
+        @display_facet = @facet_field_presenter.display_facet
+      elsif display_facet_or_field_config.is_a?(Blacklight::Configuration::Field) || field_config
+        @facet_field_presenter = nil # we need the view context to generate this
         @field_config = display_facet_or_field_config || field_config
         @display_facet = display_facet ||
                          response&.aggregations&.fetch(@field_config.field) { Blacklight::Solr::Response::Facets::NullFacetField.new(@field_config.field, response: response) }
       elsif (display_facet || display_facet_or_field_config).respond_to?(:name)
+        @facet_field_presenter = nil # we need the view context to generate this
         @display_facet = display_facet || display_facet_or_field_config
         @field_config = field_config || blacklight_config&.facet_configuration_for_field(@display_facet.name)
       else
@@ -25,6 +32,7 @@ module Blacklight
 
       @component_args = component_args
     end
+    # rubocop:enable Metrics/CyclomaticComplexity
 
     def render?
       helpers.should_render_field?(@field_config, @display_facet)
@@ -37,7 +45,7 @@ module Blacklight
 
       render(
         component.new(
-          facet_field: helpers.facet_field_presenter(@field_config, @display_facet),
+          facet_field: @facet_field_presenter || helpers.facet_field_presenter(@field_config, @display_facet),
           **@component_args
         )
       )
