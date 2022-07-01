abc68444179020eed610237838c0bdd6ec7f2c34
Chris Beer <chris@cbeer.info>
2022-07-01 08:15:24 -0700

    Backfill tests for the Blacklight::FacetComponent

--- spec/components/blacklight/facet_component_spec.rb
+++ spec/components/blacklight/facet_component_spec.rb
@@ -4,16 +4,23 @@ require 'spec_helper'
 
 RSpec.describe Blacklight::FacetComponent, type: :component do
   subject(:rendered) do
-    render_inline_to_capybara_node(described_class.new(field_config: facet_config, display_facet: display_facet ))
+    render_inline_to_capybara_node(component)
   end
+
+  let(:component) { described_class.new(**component_kwargs) }
+  let(:component_kwargs) { { field_config: facet_config, display_facet: display_facet } }
   let(:items) { [{ label: "Book", value: 'Book', hits: 20 }] }
 
   let(:display_facet) do
-    instance_double(Blacklight::Solr::Response::Facets::FacetField, items: items, limit: nil, sort: :index, offset: 0, prefix: nil)
+    instance_double(Blacklight::Solr::Response::Facets::FacetField, name: 'field', items: items, limit: nil, sort: :index, offset: 0, prefix: nil)
   end
 
-  let(:facet_config) { Blacklight::Configuration::FacetField.new(key: 'field').normalize! }
+  let(:facet_config) { Blacklight::Configuration::FacetField.new(key: 'field', component: Blacklight::FacetFieldListComponent).normalize! }
 
+  before do
+    allow(controller).to receive(:view_context).and_return(controller.view_context)
+    allow(controller.view_context).to receive(:facet_limit_for).and_return(20)
+  end
 
   it 'delegates to the configured component to render something' do
     expect(rendered).to have_selector 'ul.facet-values'
@@ -30,6 +37,66 @@ RSpec.describe Blacklight::FacetComponent, type: :component do
 
     it 'renders the partial' do
       expect(rendered).to have_content 'facet partial'
+    end
+  end
+
+  context 'with a field and response' do
+    let(:component_kwargs) do
+      { display_facet_or_field_config: facet_config, response: response }
+    end
+
+    let(:response) { instance_double(Blacklight::Solr::Response, aggregations: { 'field' => display_facet }) }
+
+    it 'extracts the facet data from the response to pass on to the rendering component' do
+      allow(facet_config.component).to receive(:new).and_call_original
+
+      rendered
+
+      expect(facet_config.component).to have_received(:new).with(facet_field: have_attributes(facet_field: facet_config, display_facet: display_facet))
+    end
+
+    context 'when the field is not in the response' do
+      let(:facet_config) { Blacklight::Configuration::FacetField.new(key: 'some_other_field', component: Blacklight::FacetFieldListComponent).normalize! }
+
+      it 'uses a null field to pass through the response information anyway' do
+        allow(facet_config.component).to receive(:new).and_call_original
+
+        rendered
+
+        expect(facet_config.component).to have_received(:new).with(facet_field: have_attributes(facet_field: facet_config, display_facet: have_attributes(items: [], response: response)))
+      end
+    end
+  end
+
+  context 'with a display facet and configuration' do
+    let(:component_kwargs) do
+      { display_facet_or_field_config: display_facet, blacklight_config: blacklight_config }
+    end
+
+    let(:blacklight_config) { Blacklight::Configuration.new.tap { |config| config.facet_fields['field'] = facet_config } }
+
+    it 'pulls the facet config from the blacklight config' do
+      allow(facet_config.component).to receive(:new).and_call_original
+
+      rendered
+
+      expect(facet_config.component).to have_received(:new).with(facet_field: have_attributes(facet_field: facet_config, display_facet: display_facet))
+    end
+  end
+
+  context 'with a presenter' do
+    let(:component_kwargs) do
+      { display_facet_or_field_config: presenter }
+    end
+
+    let(:presenter) { Blacklight::FacetFieldPresenter.new(facet_config, display_facet, controller.view_context) }
+
+    it 'renders the component with the provided presenter' do
+      allow(facet_config.component).to receive(:new).and_call_original
+
+      rendered
+
+      expect(facet_config.component).to have_received(:new).with(facet_field: presenter)
     end
   end
 end
