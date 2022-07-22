21a03421c8b393166f93bee5fd9a0f73477546fa
Marlo Longley <mlongley@stanford.edu>
2022-07-21 17:06:17 -0700

    Prefer flex to table display in facets

--- app/javascript/blacklight/index.js
+++ app/javascript/blacklight/index.js
@@ -1,6 +1,5 @@
 import BookmarkToggle from './bookmark_toggle'
 import ButtonFocus from './button_focus'
-import FacetLoad from './facet_load'
 import Modal from './modal'
 import SearchContext from './search_context'
 import Core from './core'
@@ -8,7 +7,6 @@ import Core from './core'
 export default {
   BookmarkToggle,
   ButtonFocus,
-  FacetLoad,
   Modal,
   SearchContext,
   onLoad: Core.onLoad
