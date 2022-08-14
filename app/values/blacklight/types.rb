39c87480152ab34d2210c4f45f5429c4b542cc9f
Chris Beer <chris@cbeer.info>
2022-08-14 10:53:44 -0700

    Expand on document attributes

--- app/values/blacklight/types.rb
+++ app/values/blacklight/types.rb
@@ -1,31 +1,115 @@
 # frozen_string_literal: true
 
+require "active_model"
+
 module Blacklight
   # These are data types that blacklight can use to coerce values from the index
   module Types
-    class Array
-      def self.coerce(input)
-        ::Array.wrap(input)
-      end
+    @registry = ActiveModel::Type::Registry.new
+
+    class << self
+      delegate :lookup, :register, to: :@registry
     end
 
-    class String
+    class Value
       def self.coerce(input)
-        ::Array.wrap(input).first
+        new.cast(input)
       end
-    end
 
-    class Date
-      def self.coerce(input)
-        field = String.coerce(input)
-        return if field.blank?
-
-        begin
-          ::Date.parse(field)
-        rescue ArgumentError
-          Rails.logger&.info "Unable to parse date: #{field.first.inspect}"
+      def cast(input)
+        if input.is_a?(::Array)
+          input.first
+        else
+          input
         end
       end
     end
+
+    class Array < Value
+      def initialize(of: nil, **kwargs)
+        @of = of
+        @kwargs = kwargs
+      end
+
+      def cast(input)
+        ::Array.wrap(input).map do |value|
+          lookup_type.cast(value)
+        end
+      end
+
+      private
+
+      def lookup_type
+        return Blacklight::Types::Value.new(**@kwargs) if @of.nil?
+
+        @lookup_type ||= Blacklight::Types.lookup(@of, **@kwargs)
+      end
+    end
+
+    class String < Value
+      def cast(input)
+        super.to_s
+      end
+    end
+
+    class Date < Value
+      def cast(input)
+        value = super
+        return if value.blank?
+
+        begin
+          ::Date.parse(value.to_s)
+        rescue ArgumentError
+          Rails.logger&.info "Unable to parse date: #{value.inspect}"
+        end
+      end
+    end
+
+    class Boolean < Value
+      def cast(input)
+        ActiveModel::Type::Boolean.new.cast(super)
+      end
+    end
+
+    class JsonValue < Value
+      def cast(input)
+        value = super
+
+        return value unless value.is_a?(String)
+
+        JSON.parse(value)
+      end
+    end
+
+    class Selector < Array
+      def initialize(by: nil, block: nil, **kwargs)
+        super(**kwargs)
+        @by = by
+        @block = block
+      end
+
+      def cast(input)
+        return super.public_send(@by) unless @block
+
+        super.public_send(@by, &@block)
+      end
+    end
+
+    # rubocop:disable Rails/OutputSafety
+    class Html < String
+      def cast(input)
+        super&.html_safe
+      end
+    end
+    # rubocop:enable Rails/OutputSafety
+
+    register :boolean, Boolean
+    register :string, String
+    register :date, Date
+    register :array, Array
+    register :json, JsonValue
+    register :html, Html
+    register :select, Selector
+    register :value, Value
   end
 end
