77ab8a9f0b455ffe0ebdaeef6886332d99db9fc2
Chris Beer <chris@cbeer.info>
2022-10-13 10:53:20 -0700

    Restore the wrapping blacklight-icons span to componentized icons

--- app/helpers/blacklight/icon_helper_behavior.rb
+++ app/helpers/blacklight/icon_helper_behavior.rb
@@ -9,13 +9,13 @@ module Blacklight::IconHelperBehavior
   # the svg everytime.
   # @param [String, Symbol] icon_name
   # @return [String]
-  def blacklight_icon(icon_name, _options = {})
-    render "Blacklight::Icons::#{icon_name.to_s.camelize}Component".constantize.new
+  def blacklight_icon(icon_name, **kwargs)
+    render "Blacklight::Icons::#{icon_name.to_s.camelize}Component".constantize.new(**kwargs)
   rescue NameError
     Blacklight.deprecation.warn(
       "Falling back on the LegacyIconComponent with \"#{icon_name}\" is deprecated. Instead create the component `Blacklight::Icons::#{icon_name.to_s.camelize}Component` for this icon."
     )
 
-    render Blacklight::Icons::LegacyIconComponent.new(name: icon_name)
+    render Blacklight::Icons::LegacyIconComponent.new(name: icon_name, **kwargs)
   end
 end
