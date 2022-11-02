92718e06e9e056ad922a87b2e81fada5b2cacf8d
Justin Coyne <jcoyne@justincoyne.com>
2022-11-02 08:40:00 -0500

    Extract components for the headers
    This allows us to override the header component, while reusing existing components. For example, at Stanford we would like to insert a hidden third navbar that has a feedback form between the top bar and the search navbar.  This will allow us to override the header component to do that.

---
+++ app/components/blacklight/header_component.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Blacklight
+  class HeaderComponent < Blacklight::Component
+    renders_one :top_bar, lambda { |component: Blacklight::TopNavbarComponent|
+      component.new(blacklight_config: blacklight_config)
+    }
+
+    renders_one :search_bar, lambda { |component: Blacklight::SearchNavbarComponent|
+      component.new(blacklight_config: blacklight_config)
+    }
+
+    def initialize(blacklight_config:)
+      @blacklight_config = blacklight_config
+    end
+
+    attr_reader :blacklight_config
+
+    # Hack so that the default lambdas are triggered
+    # so that we don't have to do c.top_bar() in the call.
+    def before_render
+      set_slot(:top_bar, nil) unless top_bar
+      set_slot(:search_bar, nil) unless search_bar
+    end
+  end
+end
