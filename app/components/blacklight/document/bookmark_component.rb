b4b8e57bc0b9345bb7d57e03114e56c5da20bbfe
Justin Coyne <jcoyne@justincoyne.com>
2022-11-09 08:44:30 -0600

    Update the configuration to use the component for bookmarks

--- app/components/blacklight/document/bookmark_component.rb
+++ app/components/blacklight/document/bookmark_component.rb
@@ -3,14 +3,15 @@
 module Blacklight
   module Document
     # Render a bookmark widget to bookmark / unbookmark a document
-    class BookmarkComponent < Blacklight::Component
+    class BookmarkComponent < Blacklight::Document::ActionComponent
       # @param [Blacklight::Document] document
       # @param [Boolean] checked
       # @param [Object] bookmark_path the rails route to use for bookmarks
-      def initialize(document:, checked: nil, bookmark_path: nil)
+      def initialize(document:, action: nil, checked: nil, bookmark_path: nil, **kwargs)
         @document = document
         @checked = checked
         @bookmark_path = bookmark_path
+        super(document: document, action: action, **kwargs)
       end
 
       def bookmarked?
