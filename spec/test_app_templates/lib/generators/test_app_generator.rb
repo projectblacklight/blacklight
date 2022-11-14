0b4f256b5dd51d933d0f2e428a97a012b868f16f
Chris Beer <chris@cbeer.info>
2022-11-14 11:46:02 -0800

    Use separate asset generators for importmap, sprockets, and propshaft

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -28,4 +28,10 @@ class TestAppGenerator < Rails::Generators::Base
 
     generate 'blacklight:test_support'
   end
+
+  def add_local_assets_for_propshaft
+    return unless defined?(Propshaft)
+
+    run "yarn add #{Blacklight::Engine.root}"
+  end
 end
