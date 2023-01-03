c097179073bb0ccc9c5c24d6e449aa8c0f1814aa
Benjamin Armintor <armintor@gmail.com>
2023-01-03 17:00:39 -0500

    use sessionStorage to track search and fetch pageLinks data on client in show views - use client SearchContext components when blacklight_config.track_search_session is 'client'

--- app/assets/javascripts/blacklight/blacklight.js
+++ app/assets/javascripts/blacklight/blacklight.js
@@ -333,6 +333,7 @@
 
     Blacklight.csrfToken = () => document.querySelector('meta[name=csrf-token]')?.content;
     Blacklight.csrfParam = () => document.querySelector('meta[name=csrf-param]')?.content;
+    Blacklight.searchStorage = () => document.querySelector('meta[name=blacklight-search-storage]')?.content;
 
     // this is the Rails.handleMethod with a couple adjustments, described inline:
     // first, we're attaching this directly to the event handler, so we can check for meta-keys
@@ -340,25 +341,32 @@
       const link = this;
 
       // instead of using the normal href, we need to use the context href instead
-      let href = link.getAttribute('data-context-href');
+      let contextUrl = new URL(link.getAttribute('data-context-href'), window.location);
       let target = link.getAttribute('target');
       let csrfToken = Blacklight.csrfToken();
       let csrfParam = Blacklight.csrfParam();
       let form = document.createElement('form');
-      form.method = 'post';
-      form.action = href;
+      form.action = contextUrl.pathname;
+      const formMethod = link.getAttribute('data-context-method') || 'post';
+      form.method = (formMethod == 'get') ? formMethod : 'post';
 
+      let formContent = '';
+      for (const [paramName, paramValue] of contextUrl.searchParams.entries()) {
+        formContent += `<input name="${paramName}" value="${paramValue}" type="hidden" />`;
+      }
+      if (formMethod != 'get' && formMethod != 'post') formContent += `<input name="_method" value="${formMethod}" type="hidden" />`;
 
-      let formContent = `<input name="_method" value="post" type="hidden" />
-      <input name="redirect" value="${link.getAttribute('href')}" type="hidden" />`;
-
+      if (Blacklight.searchStorage() == 'client') {
+        sessionStorage.setItem("blacklightSearch", new URLSearchParams(new URL(window.location).search));
+      } else {
+        formContent += `<input name="redirect" value="${link.getAttribute('href')}" type="hidden" />`;
+        if (csrfParam !== undefined && csrfToken !== undefined) {
+          formContent += `<input name="${csrfParam}" value="${csrfToken}" type="hidden" />`;
+        }
+      }
       // check for meta keys.. if set, we should open in a new tab
       if(event.metaKey || event.ctrlKey) {
         target = '_blank';
-      }
-
-      if (csrfParam !== undefined && csrfToken !== undefined) {
-        formContent += `<input name="${csrfParam}" value="${csrfToken}" type="hidden" />`;
       }
 
       // Must trigger submit by click on a button, else "submit" event handler won't work!
@@ -376,6 +384,68 @@
     };
 
     Blacklight.doSearchContextBehavior();
+
+    const clientAppliedParams = function(storedSearch) {
+      if (storedSearch) {
+        let backToCatalogParams = new URLSearchParams();
+        for (const [paramName, paramValue] of storedSearch.entries()) {
+          backToCatalogParams.append(paramName, paramValue);
+        }
+        const appliedParams = document.querySelector('#appliedParams');
+        if (appliedParams) {
+          const backToCatalogEle = appliedParams.querySelector('.back-to-catalog');
+          if (backToCatalogEle) {
+            const backToCatalogUrl = new URL(backToCatalogEle.href);
+            backToCatalogEle.href = `${backToCatalogUrl.pathname}?${backToCatalogParams.toString()}`;
+            backToCatalogEle.classList.remove('d-none');
+          }
+        }
+      }
+    };
+    const clientPrevNext = function(prevNextPath, storedSearch) {
+      const prevNextUrl = new URL(prevNextPath, window.location);
+      // prevNextUrl should have a counter param, but needs search params
+      let prevNextParams = new URLSearchParams(prevNextUrl.search);
+      if (storedSearch) {
+        for (const [paramName, paramValue] of storedSearch.entries()) {
+          prevNextParams.append(paramName, paramValue);
+        }
+      }
+      const setHrefOrDelete = function(linkEle, url) {
+        if (!linkEle) return;
+        if (url) {
+          linkEle.href = url;
+        } else {
+          linkEle.remove();
+        }
+      };
+      const setContent = function(ele, content) {
+        if (!ele) return;
+        ele.innerHTML = content;
+      };
+      fetch(`${prevNextUrl.pathname}?${prevNextParams.toString()}`)
+      .then((response) => response.json())
+      .then(function(responseData) {
+        if (!responseData.prev && !responseData.next) return;
+        document.querySelectorAll('.page-links').forEach(function(pageLinks) {
+          setContent(pageLinks.querySelector('.pagination-counter-raw'), responseData.counterRaw);
+          setContent(pageLinks.querySelector('.pagination-counter-delimited'), responseData.counterDelimited);
+          setContent(pageLinks.querySelector('.pagination-total-raw'), responseData.totalRaw);
+          setContent(pageLinks.querySelector('.pagination-total-delimited'), responseData.totalDelimited);
+          setHrefOrDelete(pageLinks.querySelector("a[rel='prev']"), responseData.prev);
+          setHrefOrDelete(pageLinks.querySelector("a[rel='next']"), responseData.next);
+          pageLinks.classList.remove('d-none');
+        });
+      });
+    };
+    Blacklight.onLoad(function(){
+      let clientPageLinks = document.querySelectorAll('.page-links[data-page-links-url]');
+      if (clientPageLinks[0]) {
+        const storedSearch = new URLSearchParams(sessionStorage.getItem("blacklightSearch"));
+        clientAppliedParams(storedSearch);
+        clientPrevNext(clientPageLinks[0].getAttribute('data-page-links-url'), storedSearch);
+      }
+    });
   })();
 
   const index = {
