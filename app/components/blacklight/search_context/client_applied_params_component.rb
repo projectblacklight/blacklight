4d3b762d51f0a01b422096a1c4367cd93bf4d7ca
Benjamin Armintor <armintor@gmail.com>
2023-01-04 09:02:14 -0500

    remove unnecessary permits of :counter param

--- app/components/blacklight/search_context/client_applied_params_component.rb
+++ app/components/blacklight/search_context/client_applied_params_component.rb
@@ -5,7 +5,7 @@ module Blacklight
     class ClientAppliedParamsComponent < Blacklight::Component
       delegate :search_action_path, to: :helpers
       def render?
-        controller.params.permit(:counter)[:counter]
+        controller.params[:counter]
       end
     end
   end
