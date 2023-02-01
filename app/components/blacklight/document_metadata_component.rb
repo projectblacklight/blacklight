3466f78fcfd7defaa229553beaf7212ca3c954ad
Chris Beer <chris@cbeer.info>
2023-02-01 09:19:54 -0800

    Add parameters to metadata components that change the tag, classes, and attributes

--- app/components/blacklight/document_metadata_component.rb
+++ app/components/blacklight/document_metadata_component.rb
@@ -8,17 +8,23 @@ module Blacklight
     with_collection_parameter :fields
 
     # @param fields [Enumerable<Blacklight::FieldPresenter>] Document field presenters
-    def initialize(fields: [], show: false, view_type: nil)
+    # rubocop:disable Metrics/ParameterLists
+    def initialize(fields: [], tag: 'dl', classes: %w[document-metadata dl-invert row], show: false, view_type: nil, field_layout: nil, **component_args)
       @fields = fields
+      @tag = tag
+      @classes = classes
       @show = show
       @view_type = view_type
+      @field_layout = field_layout
+      @component_args = component_args
     end
+    # rubocop:enable Metrics/ParameterLists
 
     def before_render
       return unless fields
 
       @fields.each do |field|
-        with_field(component: field.component, field: field, show: @show, view_type: @view_type)
+        with_field(component: field.component, field: field, show: @show, view_type: @view_type, layout: @field_layout)
       end
     end
 
--- app/components/blacklight/metadata_field_layout_component.rb
+++ app/components/blacklight/metadata_field_layout_component.rb
@@ -5,18 +5,21 @@ module Blacklight
     with_collection_parameter :field
     renders_one :label
     renders_many :values, (lambda do |value: nil, &block|
-      if block
-        content_tag :dd, class: "#{@value_class} blacklight-#{@key}", &block
+      if @value_tag.nil?
+        block&.call || value
+      elsif block
+        content_tag @value_tag, class: "#{@value_class} blacklight-#{@key}", &block
       else
-        content_tag :dd, value, class: "#{@value_class} blacklight-#{@key}"
+        content_tag @value_tag, value, class: "#{@value_class} blacklight-#{@key}"
       end
     end)
 
     # @param field [Blacklight::FieldPresenter]
-    def initialize(field:, label_class: 'col-md-3', value_class: 'col-md-9')
+    def initialize(field:, value_tag: 'dd', label_class: 'col-md-3', value_class: 'col-md-9')
       @field = field
       @key = @field.key.parameterize
       @label_class = label_class
+      @value_tag = value_tag
       @value_class = value_class
     end
   end
