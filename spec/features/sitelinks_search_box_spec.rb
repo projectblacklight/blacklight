33f710841566bf9ebbe0529545f717b362934b00
Benjamin Armintor <armintor@gmail.com>
2023-02-02 17:50:30 -0500

    fix spec/features/sitelinks_search_box_spec.rb

--- spec/features/sitelinks_search_box_spec.rb
+++ spec/features/sitelinks_search_box_spec.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-describe 'Sitelinks search box' do
+RSpec.describe 'Sitelinks search box' do
   it 'is home page' do
     visit root_path
-    expect(page).to have_css 'script[type="application/ld+json"]'
+    expect(page).to have_selector 'script[type="application/ld+json"]', visible: :hidden
   end
 
   it 'on search page' do
     visit search_catalog_path q: 'book'
-    expect(page).not_to have_css 'script[type="application/ld+json"]'
+    expect(page).not_to have_selector 'script[type="application/ld+json"]', visible: :hidden
   end
 end
