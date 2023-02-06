d38ee4afa8cb57e8e5a1e56730b1b3141e49de55
Chris Beer <chris@cbeer.info>
2023-02-06 14:22:15 -0800

    Allow Blacklight to define Stimulus controllers

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
 pin_all_from File.expand_path("../app/javascript/blacklight", __dir__), under: "blacklight"
+pin_all_from File.expand_path("../app/javascript/controllers", __dir__), under: "controllers"
