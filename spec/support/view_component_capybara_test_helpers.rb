7024324d48052b7aa2bb67111e5a8eba2186572f
Chris Beer <chris@cbeer.info>
2023-02-21 08:38:17 -0800

    Continue to provide access to the view component's controller; hacky fix for #3013

--- spec/support/view_component_capybara_test_helpers.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module ViewComponentCapybaraTestHelpers
-  # Work around for https://github.com/teamcapybara/capybara/issues/2466
-  def render_inline_to_capybara_node(component)
-    Capybara::Node::Simple.new(render_inline(component).to_s)
-  end
-end
