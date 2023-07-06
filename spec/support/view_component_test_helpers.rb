1eb3ba2a4679ff24ed139dda9fabb2dafc6c24b3
Benjamin Armintor <armintor@gmail.com>
2023-07-06 17:37:35 -0400

    Accommodate variable libxml2 HTML parsing behavior in spec/components/blacklight/facet_item_pivot_component_spec.rb - fixes #3058

--- spec/support/view_component_test_helpers.rb
+++ spec/support/view_component_test_helpers.rb
@@ -18,4 +18,18 @@ module ViewComponentTestHelpers
 
     ApplicationController.new.extend(Rails.application.routes.url_helpers)
   end
+
+  # Nokogiri 1.15.0 upgrades the vendored libxml2 from v2.10.4 to v2.11.3
+  # libxml2 v2.11.0 introduces a change to parsing HTML href attributes
+  # in nokogiri < 1.15, brackets in href attributes are escaped:
+  # - <a class="facet-select" rel="nofollow" href="/catalog?f%5Bz%5D%5B%5D=x:1">x:1</a>
+  # in nokogiri >= 1.15, brackets in href attributes are not escaped:
+  # - <a class="facet-select" rel="nofollow" href="/catalog?f[z][]=x:1">x:1</a>
+  # until we can spec a minimum nokogiri version of 1.15.0, we need to see how
+  # the installed version parsed the html
+  def nokogiri_mediated_href(href)
+    start = "<a href=\"".length
+    stop = -"\"></a>".length
+    Nokogiri::HTML.fragment("<a href=\"#{href}\"></a>").to_s[start...stop]
+  end
 end
