223ec9d95c5ef4c9bce23a4d4be21de89196c85e
Carla Galarza <cgalarza@upenn.edu>
2023-10-12 11:27:38 -0400

    Adding documentation.

--- app/presenters/blacklight/facet_field_range_presenter.rb
+++ app/presenters/blacklight/facet_field_range_presenter.rb
@@ -5,6 +5,7 @@ module Blacklight
     delegate :response, to: :display_facet
     delegate :blacklight_config, to: :search_state
 
+    # Paginator will return the selected item or if no facet is selected, the [Missing] facet.
     def paginator
       return unless display_facet
 
@@ -21,12 +22,17 @@ module Blacklight
       values&.first
     end
 
+    # Wraps selected range in Blacklight::Solr::Response::Facets::FacetItem object.
+    #
+    # @return [Blacklight::Solr::Response::Facets::FacetItem] if range is selected
+    # @return [NilClass] if no range is selected
     def selected_item
       return unless selected_range
 
       Blacklight::Solr::Response::Facets::FacetItem.new(value: selected_range, hits: response.total)
     end
 
+    # Returns true if [Missing] facet is selected.
     def missing_selected?
       selected_range == Blacklight::SearchState::FilterField::MISSING
     end
