af8b821ef3ab73d438742942dedb32b23119bb6e
Jonathan Rochkind <jonathan@dnil.net>
2023-11-08 14:56:38 -0500

    Avoid deprecation notice on ActiveSupport::Deprecation::DeprecatedObjectProxy by passing specific deprecation object with correct gem and version
    DEPRECATION WARNING: DeprecatedObjectProxy without a deprecator is deprecated

--- app/controllers/concerns/blacklight/bookmarks.rb
+++ app/controllers/concerns/blacklight/bookmarks.rb
@@ -42,7 +42,11 @@ module Blacklight::Bookmarks
     @bookmarks = token_or_current_or_guest_user.bookmarks
     bookmark_ids = @bookmarks.collect { |b| b.document_id.to_s }
     @response, deprecated_document_list = search_service.fetch(bookmark_ids)
-    @document_list = ActiveSupport::Deprecation::DeprecatedObjectProxy.new(deprecated_document_list, "The @document_list instance variable is now deprecated and will be removed in Blacklight 8.0")
+    @document_list = ActiveSupport::Deprecation::DeprecatedObjectProxy.new(
+      deprecated_document_list,
+      "The @document_list instance variable is now deprecated",
+      ActiveSupport::Deprecation.new("8.0", "blacklight")
+    )
 
     respond_to do |format|
       format.html {}
