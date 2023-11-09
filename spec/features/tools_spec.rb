a5ab6ad7e30040a7009299bdd184a14fe3d8a42f
Patrick Perkins <lilblizzard97@gmail.com>
2023-11-09 11:39:59 -0500

    revise capybara spec

--- spec/features/tools_spec.rb
+++ spec/features/tools_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe 'Tools' do
   before { visit solr_document_path('2007020969') }
   it 'displays Email modal properly' do
     click_link 'Email'
-    expect(find('div.modal-header')).to have_text I18n.t('blacklight.email.form.title')
-    expect(find('div.modal-body')).to have_text I18n.t('blacklight.email.form.to')
+    expect(find('div.modal-header')).to have_text 'Email This'
+    expect(page).to have_selector('form#email_form')
   end
 
   it 'displays SMS modal properly' do
     click_link 'SMS'
-    expect(find('div.modal-header')).to have_text I18n.t('blacklight.sms.form.title')
-    expect(find('div.modal-body')).to have_text I18n.t('blacklight.sms.form.to')
+    expect(find('div.modal-header')).to have_text 'SMS This'
+    expect(page).to have_selector('form#sms_form')
   end
 end
