a0b51399eb52c97fe65c37b9ea926226c6e3e3c3
Patrick Perkins <lilblizzard97@gmail.com>
2023-11-09 16:09:07 -0500

    fix citation header rendering, update specs

--- spec/features/tools_spec.rb
+++ spec/features/tools_spec.rb
@@ -2,15 +2,21 @@
 
 RSpec.describe 'Tools' do
   before { visit solr_document_path('2007020969') }
-  it 'displays Email modal properly' do
+
+  it 'displays Email modal with form' do
     click_link 'Email'
     expect(find('div.modal-header')).to have_text 'Email This'
     expect(page).to have_selector('form#email_form')
   end
 
-  it 'displays SMS modal properly' do
+  it 'displays SMS modal with form' do
     click_link 'SMS'
     expect(find('div.modal-header')).to have_text 'SMS This'
     expect(page).to have_selector('form#sms_form')
+  end
+
+  it 'displays the Cite modal with expected header' do
+    click_link 'Cite'
+    expect(find('div.modal-header')).to have_text 'Cite'
   end
 end
