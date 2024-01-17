17b311b22b87a7c70835bd8df218cc9426f8f4f9
Justin Coyne <jcoyne@justincoyne.com>
2024-01-17 13:32:56 -0600

    Use bootstrap 5.3 for newly generated apps

--- lib/generators/blacklight/assets_generator.rb
+++ lib/generators/blacklight/assets_generator.rb
@@ -2,7 +2,7 @@
 
 module Blacklight
   class AssetsGenerator < Rails::Generators::Base
-    class_option :'bootstrap-version', type: :string, default: ENV.fetch('BOOTSTRAP_VERSION', '~> 5.1'), desc: "Set the generated app's bootstrap version"
+    class_option :'bootstrap-version', type: :string, default: ENV.fetch('BOOTSTRAP_VERSION', '~> 5.3'), desc: "Set the generated app's bootstrap version"
 
     def run_asset_pipeline_specific_generator
       generated_options = "--bootstrap-version='#{options[:'bootstrap-version']}'" if options[:'bootstrap-version']
