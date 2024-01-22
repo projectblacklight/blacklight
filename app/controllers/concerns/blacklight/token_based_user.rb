affab1dc1d54bcbcf0afe016bf62ca79c1feb9b1
Justin Coyne <jcoyne@justincoyne.com>
2024-01-22 07:25:28 -0600

    Simplify encrypting and decrypting tokens

--- app/controllers/concerns/blacklight/token_based_user.rb
+++ app/controllers/concerns/blacklight/token_based_user.rb
@@ -28,7 +28,7 @@ module Blacklight::TokenBasedUser
 
   # Used for #export action, with encrypted user_id.
   def decrypt_user_id(encrypted_user_id)
-    user_id, timestamp = message_encryptor.decrypt_and_verify(encrypted_user_id)
+    user_id, timestamp = Rails::Secrets.decrypt(encrypted_user_id)
 
     if timestamp < 1.hour.ago
       raise Blacklight::Exceptions::ExpiredSessionToken
@@ -41,27 +41,6 @@ module Blacklight::TokenBasedUser
   # as a helper method for views.
   def encrypt_user_id(user_id, current_time = nil)
     current_time ||= Time.zone.now
-    message_encryptor.encrypt_and_sign([user_id, current_time])
-  end
-
-  def export_secret_token
-    secret_key_generator.generate_key('encrypted user session key')[0..(key_len - 1)]
-  end
-
-  def secret_key_generator
-    @secret_key_generator ||= ActiveSupport::KeyGenerator.new(Rails.application.secret_key_base)
-  end
-
-  def message_encryptor
-    ActiveSupport::MessageEncryptor.new(export_secret_token)
-  end
-
-  # Ruby 2.4 requires keys of very particular lengths
-  def key_len
-    if ActiveSupport::MessageEncryptor.respond_to? :key_len
-      ActiveSupport::MessageEncryptor.key_len
-    else
-      0
-    end
+    Rails::Secrets.encrypt([user_id, current_time])
   end
 end
