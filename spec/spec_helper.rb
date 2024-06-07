a25373b0efe894713c94793c234aa5619d4c894d
Justin Coyne <jcoyne@justincoyne.com>
2024-06-07 09:48:24 -0500

    Update target ruby version for rubocop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,7 +41,7 @@ end
 # in spec/support/ and its subdirectories.
 # Blacklight, again, make sure we're looking in the right place for em.
 # Relative to HERE, NOT to Rails.root, which is off somewhere else.
-Dir[Pathname.new(File.expand_path('support/**/*.rb', __dir__))].sort.each { |f| require f }
+Dir[Pathname.new(File.expand_path('support/**/*.rb', __dir__))].each { |f| require f }
 
 RSpec.configure do |config|
   config.disable_monkey_patching!
