0e4b4d1fa336814bd9c2711de478ea31e5913ff8
Justin Coyne <jcoyne@justincoyne.com>
2024-06-20 10:34:20 -0500

    Explicitly set the margin class on the constraint component
    This makes it easier to override the margin if necessary

--- app/components/blacklight/constraint_component.rb
+++ app/components/blacklight/constraint_component.rb
@@ -4,7 +4,7 @@ module Blacklight
   class ConstraintComponent < Blacklight::Component
     with_collection_parameter :facet_item_presenter
 
-    def initialize(facet_item_presenter:, classes: 'filter', layout: Blacklight::ConstraintLayoutComponent)
+    def initialize(facet_item_presenter:, classes: %w[filter mx-1], layout: Blacklight::ConstraintLayoutComponent)
       @facet_item_presenter = facet_item_presenter
       @classes = classes
       @layout = layout
