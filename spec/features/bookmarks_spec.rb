7983a7ba8262abaa9abe0240f55234ffe223e6bd
Amrey Mathurin <mamrey@upenn.edu>
2024-06-25 15:57:00 -0400

    Address rubocop

--- spec/features/bookmarks_spec.rb
+++ spec/features/bookmarks_spec.rb
@@ -12,10 +12,10 @@ RSpec.describe "Bookmarks" do
 
   describe "when bookmark counter is not rendered" do
     before do
-        CatalogController.configure_blacklight do |config|
-          config.add_nav_action(:bookmark, partial: 'blacklight/nav/bookmark', if: proc { false })
-        end
+      CatalogController.configure_blacklight do |config|
+        config.add_nav_action(:bookmark, partial: 'blacklight/nav/bookmark', if: proc { false })
       end
+    end
 
     it 'adds bookmark without raising an alert', :js do
       visit solr_document_path('2007020969')
