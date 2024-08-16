5f33d5e58eee0502e37a25ea672aa2417741b8ef
Justin Coyne <jcoyne@justincoyne.com>
2024-08-16 13:24:41 -0500

    Extract DropdownButtonCompnent

--- app/components/blacklight/system/dropdown_component.rb
+++ app/components/blacklight/system/dropdown_component.rb
@@ -3,11 +3,8 @@
 module Blacklight
   module System
     class DropdownComponent < Blacklight::Component
-      renders_one :button, (lambda do |classes:, label:|
-        button_tag class: classes, aria: { expanded: false }, data: { toggle: 'dropdown', 'bs-toggle': 'dropdown' } do
-          safe_join([label, content_tag(:span, '', class: 'caret')])
-        end
-      end)
+      renders_one :button, DropdownButtonComponent
+
       renders_many :options, (lambda do |text:, url:, selected: false|
         link_to(text, url, class: "dropdown-item #{'active' if selected}", role: 'menuitem', aria: { current: ('page' if selected) })
       end)
@@ -29,7 +26,7 @@ module Blacklight
       end
 
       def before_render
-        with_button(classes: 'btn btn-outline-secondary dropdown-toggle', label: button_label) unless button
+        with_button(label: button_label) unless button
 
         return if options.any?
 
