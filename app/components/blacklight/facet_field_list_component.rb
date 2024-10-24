07aa8d426ef810dba21e8660e567462049610387
Justin Coyne <jcoyne@justincoyne.com>
2024-10-24 08:56:42 -0500

    Remove FacetFieldListComponent#render_facet_limit_list

--- app/components/blacklight/facet_field_list_component.rb
+++ app/components/blacklight/facet_field_list_component.rb
@@ -7,24 +7,6 @@ module Blacklight
       @layout = layout == false ? FacetFieldNoLayoutComponent : Blacklight::FacetFieldComponent
     end
 
-    ##
-    # Renders the list of values
-    # removes any elements where render_facet_item returns a nil value. This enables an application
-    # to filter undesireable facet items so they don't appear in the UI
-    # @deprecated
-    def render_facet_limit_list(paginator, facet_field, wrapping_element = :li)
-      return render(facet_items(wrapping_element: wrapping_element)) if paginator == @facet_field.paginator && facet_field == @facet_field.key
-
-      facet_config ||= helpers.facet_configuration_for_field(facet_field)
-
-      collection = paginator.items.map do |item|
-        facet_item_presenter(item, facet_config, facet_field)
-      end
-
-      render(facet_item_component_class(facet_config).with_collection(collection, wrapping_element: wrapping_element))
-    end
-    Blacklight.deprecation.deprecate_methods(self, render_facet_limit_list: 'Call e.g. `render facet_items` instead')
-
     def facet_items(wrapping_element: :li, **item_args)
       facet_item_component_class.with_collection(facet_item_presenters, wrapping_element: wrapping_element, **item_args)
     end
