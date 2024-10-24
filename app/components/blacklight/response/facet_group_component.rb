528dcf6ade9333110a48187d71177bef46fc698e
Justin Coyne <jcoyne@justincoyne.com>
2024-10-24 09:31:40 -0500

    Remove deprecated arguments from FacetGroupComponent#initialize

--- app/components/blacklight/response/facet_group_component.rb
+++ app/components/blacklight/response/facet_group_component.rb
@@ -6,19 +6,13 @@ module Blacklight
     class FacetGroupComponent < Blacklight::Component
       renders_one :body
 
-      # @param [Blacklight::Response] response
-      # @param [Array<Blacklight::Configuration::FacetField>] fields facet fields to render
-      # @param [String] title the title of the facet group section
       # @param [String] id a unique identifier for the group
-      def initialize(id:, title: nil, fields: [], response: nil)
+      # @param [String] title the title of the facet group section
+      def initialize(id:, title: nil)
         @groupname = id
         @id = id ? "facets-#{id}" : 'facets'
         @title = title || I18n.t("blacklight.search.#{@id}.title")
         @panel_id = id ? "facet-panel-#{id}-collapse" : 'facet-panel-collapse'
-
-        # deprecated variables
-        @fields = fields
-        @response = response
       end
 
       def render?
