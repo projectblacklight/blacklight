31f4e44124f8a9882f52a311cfb383fd3c2df632
Justin Coyne <jcoyne@justincoyne.com>
2024-10-24 09:19:27 -0500

    Remove deprecated legacy icons

--- app/helpers/blacklight/icon_helper_behavior.rb
+++ app/helpers/blacklight/icon_helper_behavior.rb
@@ -4,18 +4,10 @@
 # Module to help generate icon helpers for SVG images
 module Blacklight::IconHelperBehavior
   ##
-  # Returns the raw SVG (String) for a Blacklight Icon located in
-  # app/assets/images/blacklight/*.svg. Caches them so we don't have to look up
-  # the svg everytime.
+  # Returns the raw SVG (String) for a Blacklight Icon
   # @param [String, Symbol] icon_name
   # @return [String]
   def blacklight_icon(icon_name, **kwargs)
     render "Blacklight::Icons::#{icon_name.to_s.camelize}Component".constantize.new(**kwargs)
-  rescue NameError
-    Blacklight.deprecation.warn(
-      "Falling back on the LegacyIconComponent with \"#{icon_name}\" is deprecated. Instead create the component `Blacklight::Icons::#{icon_name.to_s.camelize}Component` for this icon."
-    )
-
-    render Blacklight::Icons::LegacyIconComponent.new(name: icon_name, **kwargs)
   end
 end
