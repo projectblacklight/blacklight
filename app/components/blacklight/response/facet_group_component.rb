d8728eb3959f3eca5862df15120870b8bf0265f8
Justin Coyne <jcoyne@justincoyne.com>
2024-10-24 09:29:07 -0500

    Remove deprecated FacetGroupComponent#default_body

--- app/components/blacklight/response/facet_group_component.rb
+++ app/components/blacklight/response/facet_group_component.rb
@@ -21,26 +21,8 @@ module Blacklight
         @response = response
       end
 
-      # Provide fallback behavior for rendering this object without a body slot
-      def before_render
-        set_slot(:body, nil) { default_body } unless body?
-      end
-
       def render?
         body.present?
-      end
-
-      private
-
-      # @deprecated
-      def default_body
-        Blacklight.deprecation.warn('Rendering the Blacklight::FacetGroupComponent without a body slot is deprecated.')
-        render(Blacklight::FacetComponent.with_collection(@fields, response: @response))
-      end
-
-      # @deprecated
-      def blacklight_config
-        helpers.blacklight_config
       end
     end
   end
