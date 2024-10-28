226dc0477ea8ee0b8230224bf315a3fe6538627c
dnoneill <noneill@stanford.edu>
2024-10-28 12:11:47 -0400

    update link_to_search to work with only one search_field

--- app/components/blacklight/skip_link_component.rb
+++ app/components/blacklight/skip_link_component.rb
@@ -3,11 +3,17 @@
 module Blacklight
   class SkipLinkComponent < Blacklight::Component
     def link_to_search
-      link_to t('blacklight.skip_links.search_field'), '#search_field', class: link_classes
+      link_to t('blacklight.skip_links.search_field'), search_id, class: link_classes
     end
 
     def link_to_main
       link_to t('blacklight.skip_links.main_content'), '#main-container', class: link_classes
+    end
+
+    def search_id
+      return '#search_field' if helpers.blacklight_config.search_fields.values.many? { |field_def| helpers.should_render_field?(field_def) }
+
+      '#q'
     end
 
     def link_classes
