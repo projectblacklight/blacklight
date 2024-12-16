086db0370c52a8bdbebaf9cc9ee5f00e8fd246e0
Jane Sandberg <sandbergja@users.noreply.github.com>
2024-12-16 07:42:59 -0800

    Update app/javascript/blacklight/debounce.js
    Co-authored-by: Max Kadel <mkadel@princeton.edu>

--- app/javascript/blacklight/debounce.js
+++ app/javascript/blacklight/debounce.js
@@ -3,7 +3,7 @@
 // const basicFunction = (entry) => console.log(entry)
 // const debounced = debounce(basicFunction("I should only be called once"));
 //
-// debounced // does NOT print to the screen becase it is invoked again less than 200 milliseconds later
+// debounced // does NOT print to the screen because it is invoked again less than 200 milliseconds later
 // debounced // does print to the screen
 // ```
 export default function debounce(func, timeout = 200) {
