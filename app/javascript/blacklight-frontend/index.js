bc9d968edb69836f70e990a5904575c4e0a0f0bd
Justin Coyne <jcoyne@justincoyne.com>
2025-04-23 13:08:22 -0700

    Remove unused modalForm

--- app/javascript/blacklight-frontend/index.js
+++ app/javascript/blacklight-frontend/index.js
@@ -2,7 +2,6 @@ import BookmarkToggle from 'blacklight-frontend/bookmark_toggle'
 import ButtonFocus from 'blacklight-frontend/button_focus'
 import FacetSuggest from 'blacklight-frontend/facet_suggest'
 import Modal from 'blacklight-frontend/modal'
-import ModalForm from 'blacklight-frontend/modalForm'
 import SearchContext from 'blacklight-frontend/search_context'
 import Core from 'blacklight-frontend/core'
 
@@ -11,7 +10,6 @@ export default {
   ButtonFocus,
   FacetSuggest,
   Modal,
-  ModalForm,
   SearchContext,
   Core,
   onLoad: Core.onLoad
