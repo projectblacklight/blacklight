300f2502d0b0d2c7ac3e068e55ae2bf4df43ce7c
Justin Coyne <jcoyne@justincoyne.com>
2025-05-13 17:05:39 -0500

    Simplify FacetSuggestInput by only passing the presenter argument
    The facet is part of the presenter, we don't need to pass both

--- spec/components/blacklight/search/facet_suggest_input_spec.rb
+++ spec/components/blacklight/search/facet_suggest_input_spec.rb
@@ -4,46 +4,54 @@ require 'spec_helper'
 
 RSpec.describe Blacklight::Search::FacetSuggestInput, type: :component do
   let(:facet) { Blacklight::Configuration::FacetField.new key: 'language_facet', suggest: true }
-  let(:presenter) { instance_double(Blacklight::FacetFieldPresenter) }
-  let(:view_context) { double(ActionView::Base) }
+  let(:presenter) { Blacklight::FacetFieldPresenter.new(facet, nil, nil, nil) }
+  let(:component) { described_class.new(presenter: presenter) }
 
   before do
-    allow(presenter).to receive_messages(label: 'Language', view_context: view_context)
-    allow(view_context).to receive(:search_facet_path).and_return('/catalog/facet/language_facet')
+    allow(presenter).to receive_messages(label: 'Language')
   end
 
   it 'has an input with the facet-suggest class, which the javascript needs to find it' do
-    rendered = render_inline(described_class.new(facet: facet, presenter: presenter))
-    expect(rendered.css("input.facet-suggest").count).to eq 1
+    with_request_url "/catalog/facet/language_facet" do
+      rendered = render_inline component
+      expect(rendered.css("input.facet-suggest").count).to eq 1
+    end
   end
 
   it 'has an input with the data-facet-field attribute, which the javascript needs to determine the correct query' do
-    rendered = render_inline(described_class.new(facet: facet, presenter: presenter))
-    expect(rendered.css('input[data-facet-field="language_facet"]').count).to eq 1
+    with_request_url "/catalog/facet/language_facet" do
+      rendered = render_inline component
+      expect(rendered.css('input[data-facet-field="language_facet"]').count).to eq 1
+    end
   end
 
   it 'has an input with the data-facet-search-context attribute, which the javascript needs to determine the current search context' do
-    allow(view_context).to receive(:search_facet_path).and_return('/catalog/facet/language_facet?f%5Bformat%5D%5B%5D=Book&facet.prefix=R&facet.sort=index&q=tibet&search_field=all_fields')
-    rendered = render_inline(described_class.new(facet: facet, presenter: presenter))
-    expect(rendered.css('input[data-facet-search-context="/catalog/facet/language_facet?f%5Bformat%5D%5B%5D=Book&facet.prefix=R&facet.sort=index&q=tibet&search_field=all_fields"]').count).to eq 1
+    with_request_url "/catalog/facet/language_facet?f%5Bformat%5D%5B%5D=Book&facet.prefix=R&facet.sort=index&q=tibet&search_field=all_fields" do
+      rendered = render_inline component
+      expect(rendered.css('input[data-facet-search-context="/catalog/facet/language_facet.html?f%5Bformat%5D%5B%5D=Book&facet.prefix=R&facet.sort=index&q=tibet&search_field=all_fields"]').count).to eq 1
+    end
   end
 
   it 'has a visible label that is associated with the input' do
-    rendered = render_inline(described_class.new(facet: facet, presenter: presenter))
-    label = rendered.css('label').first
-    expect(label.text.strip).to eq 'Filter Language'
+    with_request_url "/catalog/facet/language_facet" do
+      rendered = render_inline component
+      label = rendered.css('label').first
+      expect(label.text.strip).to eq 'Filter Language'
 
-    id_in_label_for = label.attribute('for').text
-    expect(id_in_label_for).to eq('facet_suggest_language_facet')
+      id_in_label_for = label.attribute('for').text
+      expect(id_in_label_for).to eq('facet_suggest_language_facet')
 
-    expect(rendered.css('input').first.attribute('id').text).to eq id_in_label_for
+      expect(rendered.css('input').first.attribute('id').text).to eq id_in_label_for
+    end
   end
 
   context 'when the facet is explicitly configured to suggest: false' do
     let(:facet) { Blacklight::Configuration::FacetField.new key: 'language_facet', suggest: false }
 
     it 'does not display' do
-      expect(render_inline(described_class.new(facet: facet, presenter: presenter)).to_s).to eq ''
+      with_request_url "/catalog/facet/language_facet" do
+        expect(render_inline(component).to_s).to eq ''
+      end
     end
   end
 
@@ -51,8 +59,10 @@ RSpec.describe Blacklight::Search::FacetSuggestInput, type: :component do
     let(:facet) { Blacklight::Configuration::FacetField.new key: 'language_facet' }
 
     it 'displays' do
-      rendered = render_inline(described_class.new(facet: facet, presenter: presenter))
-      expect(rendered.css("input.facet-suggest").count).to eq 1
+      with_request_url "/catalog/facet/language_facet" do
+        rendered = render_inline component
+        expect(rendered.css("input.facet-suggest").count).to eq 1
+      end
     end
   end
 end
