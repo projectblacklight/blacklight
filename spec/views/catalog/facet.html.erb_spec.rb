963787eafa517ea86be250ba088f0d05f5fd8412
Justin Coyne <jcoyne@justincoyne.com>
2025-05-14 14:03:10 -0500

    Get rid of unnecessary _facet_pagination template
    call the component directly

--- spec/views/catalog/facet.html.erb_spec.rb
+++ spec/views/catalog/facet.html.erb_spec.rb
@@ -5,17 +5,20 @@ RSpec.describe 'catalog/facet.html.erb' do
   let(:blacklight_config) { Blacklight::Configuration.new }
   let(:component) { instance_double(Blacklight::FacetComponent) }
   let(:facet_suggest_input) { instance_double(Blacklight::Search::FacetSuggestInput) }
+  let(:pagination) { instance_double(Blacklight::FacetFieldPaginationComponent) }
 
   before do
     allow(Blacklight::FacetComponent).to receive(:new).and_return(component)
     allow(Blacklight::Search::FacetSuggestInput).to receive(:new).and_return(facet_suggest_input)
+    allow(Blacklight::FacetFieldPaginationComponent).to receive(:new).and_return(pagination)
+
     allow(view).to receive(:render).and_call_original
     allow(view).to receive(:render).with(component)
     allow(view).to receive(:render).with(facet_suggest_input)
+    allow(view).to receive(:render).with(pagination)
 
     blacklight_config.add_facet_field 'xyz', label: "Facet title"
     allow(view).to receive(:blacklight_config).and_return(blacklight_config)
-    stub_template 'catalog/_facet_pagination.html.erb' => 'pagination'
     assign :facet, blacklight_config.facet_fields['xyz']
     assign :display_facet, display_facet
   end
@@ -25,14 +28,10 @@ RSpec.describe 'catalog/facet.html.erb' do
     expect(rendered).to have_css 'h1', text: "Facet title"
   end
 
-  it "renders the facet suggest input" do
+  it "renders the subcomponents" do
     render
     expect(view).to have_received(:render).with(facet_suggest_input)
-  end
-
-  it "renders facet pagination" do
-    render
-    expect(rendered).to have_content 'pagination'
+    expect(view).to have_received(:render).with(pagination).twice
   end
 
   it "renders the facet limit" do
