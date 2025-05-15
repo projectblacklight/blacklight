5db2b72d766809f5bcc5d549c0cf5986d497fd02
Justin Coyne <jcoyne@justincoyne.com>
2025-05-15 17:32:51 -0500

    Allow setting classes on FacetGroupComponent
    Fixes #2733

--- app/components/blacklight/response/facet_group_component.rb
+++ app/components/blacklight/response/facet_group_component.rb
@@ -8,12 +8,15 @@ module Blacklight
 
       # @param [String] id a unique identifier for the group
       # @param [String] title the title of the facet group section
-      def initialize(id:, title: nil)
+      def initialize(id:, title: nil, body_classes: 'facets-collapse d-lg-block collapse accordion')
         @groupname = id
         @id = id ? "facets-#{id}" : 'facets'
         @title = title || I18n.t("blacklight.search.#{@id}.title")
         @panel_id = id ? "facet-panel-#{id}-collapse" : 'facet-panel-collapse'
+        @body_classes = body_classes
       end
+
+      attr_accessor :body_classes
 
       def collapse_toggle_button(panel_id)
         render button_component.new(panel_id: panel_id)
@@ -24,6 +27,7 @@ module Blacklight
       end
 
       def render?
+        # debugger
         body.present?
       end
     end
