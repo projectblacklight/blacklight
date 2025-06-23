e7833aeafdf80e3d83d5280af4bf180789391a6d
Justin Coyne <jcoyne@justincoyne.com>
2025-06-23 16:07:11 -0500

    Allow components to be passed into facets/filters_component

--- app/components/blacklight/facets/filters_component.rb
+++ app/components/blacklight/facets/filters_component.rb
@@ -3,16 +3,20 @@
 module Blacklight::Facets
   class FiltersComponent < Blacklight::Component
     # @param [Blacklight::FacetFieldPresenter] presenter
-    def initialize(presenter:, classes: 'facet-filters card card-body bg-light p-3 mb-3 border-0')
+    def initialize(presenter:, classes: 'facet-filters card card-body bg-light p-3 mb-3 border-0',
+                   suggestions_component: Blacklight::Facets::SuggestComponent,
+                   index_navigation_component: Blacklight::Facets::IndexNavigationComponent)
       @presenter = presenter
       @classes = classes
+      @suggestions_component = suggestions_component
+      @index_navigation_component = index_navigation_component
     end
 
     def facet
       @presenter.facet_field
     end
 
-    attr_reader :classes, :presenter
+    attr_reader :classes, :presenter, :suggestions_component, :index_navigation_component
 
     delegate :display_facet, to: :presenter
 
@@ -22,6 +26,14 @@ module Blacklight::Facets
 
     def render_index_navigation?
       facet.index_range && display_facet.index?
+    end
+
+    def suggestions
+      render suggestions_component.new(presenter: presenter)
+    end
+
+    def index_navigation
+      render index_navigation_component.new(presenter: presenter)
     end
   end
 end
