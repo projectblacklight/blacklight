d2ca525d393534d6bde98974f09c5594f486e16d
Justin Coyne <jcoyne@justincoyne.com>
2025-07-30 12:03:12 -0500

    Update to view_component 4.0

--- spec/components/blacklight/facets/index_navigation_component_spec.rb
+++ spec/components/blacklight/facets/index_navigation_component_spec.rb
@@ -16,7 +16,8 @@ RSpec.describe Blacklight::Facets::IndexNavigationComponent, type: :component do
 
   it 'renders the facet index navigation range' do
     expect(page).to have_css '.pagination'
-    expect(page).to have_link '0', href: '/catalog/facet/language.html?facet.prefix=0&facet.sort=index'
+    facet_path = ViewComponent::VERSION::MAJOR == 3 ? '/catalog/facet/language.html' : '/catalog/facet/language'
+    expect(page).to have_link '0', href: "#{facet_path}?facet.prefix=0&facet.sort=index"
     expect(page).to have_link '1'
     expect(page).to have_link '8'
     expect(page).to have_link '9'
