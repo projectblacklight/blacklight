0378d3789e88c17712ca15f220723435a8049840
David Kinzer <dtkinzer@gmail.com>
2026-04-23 20:31:10 -0400

    Include js tag only if available.

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 module BlacklightHelper
   include Blacklight::BlacklightHelperBehavior
+
+  def javascript_include_tag_if_exists(source, **options)
+    javascript_include_tag(source, **options) if asset_exists?("#{source}.js")
+  end
+
+  private
+
+  def asset_exists?(logical_path)
+    Rails.application.assets&.resolver&.resolve(logical_path).present?
+  rescue StandardError
+    false
+  end
 end
