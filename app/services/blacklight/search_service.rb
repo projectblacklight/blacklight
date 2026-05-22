4bf32e71193b34ae3271eef49889e0d34b176268
Justin Coyne <jcoyne@justincoyne.com>
2026-05-22 00:12:22 -0500

    Remove unused method

--- app/services/blacklight/search_service.rb
+++ app/services/blacklight/search_service.rb
@@ -18,10 +18,6 @@ module Blacklight
       search_builder_class.new(self)
     end
 
-    def search_state_class
-      @search_state.class
-    end
-
     # Fetch query results from solr
     # @yield [search_builder] optional block yields configured SearchBuilder,  caller can modify or create new
     #                         SearchBuilder to be used. Block should return SearchBuilder to be used.
